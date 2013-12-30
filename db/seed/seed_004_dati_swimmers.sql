be38a234273bbb979727e3e906fb020360e28715
Marco Ligabue <leegaweb@gmail.com>
2013-12-30 20:45:37 +0100

    Correct seed problems with badges. No changes on DB structure

--- db/seed/seed_004_dati_swimmers.sql
+++ db/seed/seed_004_dati_swimmers.sql
@@ -6817,9 +6817,5 @@ INSERT INTO badges (id,season_id,team_id,swimmer_id,number,user_id,category_type
 
 INSERT INTO badges (id,season_id,team_id,swimmer_id,number,user_id,category_type_id,entry_time_type_id,team_affiliation_id,lock_version,created_at,updated_at) VALUES 
 (4601,131,1,97,'AT04228112',2,(select t.id from category_types t where t.code = 'M40' and t.season_id = 131),(select t.id from entry_time_types t where t.code = 'O'),(select ta.id from team_affiliations ta where ta.team_id = 1 and ta.season_id = 131),0,CURDATE(),CURDATE()),
-(4602,131,32,1315,'?',2,(select t.id from category_types t where t.code = 'M35' and t.season_id = 131),(select t.id from entry_time_types t where t.code = 'M'),(select ta.id from team_affiliations ta where ta.team_id = 32 and ta.season_id = 131),0,CURDATE(),CURDATE()),
-(4603,131,11,1691,'?',2,(select t.id from category_types t where t.code = 'M45' and t.season_id = 131),(select t.id from entry_time_types t where t.code = 'M'),(select ta.id from team_affiliations ta where ta.team_id = 11 and ta.season_id = 131),0,CURDATE(),CURDATE()),
-(4604,131,14,1807,'?',2,(select t.id from category_types t where t.code = 'M35' and t.season_id = 131),(select t.id from entry_time_types t where t.code = 'M'),(select ta.id from team_affiliations ta where ta.team_id = 14 and ta.season_id = 131),0,CURDATE(),CURDATE()),
-(4605,131,1,97,'AT04228112',2,(select t.id from category_types t where t.code = 'M40' and t.season_id = 131),(select t.id from entry_time_types t where t.code = 'O'),(select ta.id from team_affiliations ta where ta.team_id = 1 and ta.season_id = 131),0,CURDATE(),CURDATE());
 
 COMMIT;
