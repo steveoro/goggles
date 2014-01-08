008d232511bfa5bd5b5b8db287e442be5bc6bc4d
Stefano Alloro <steve.alloro@gmail.com>
2014-01-08 13:46:30 +0100

    Locale files update

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -3,8 +3,8 @@ module Version
   # ** DO NOT CHANGE THE FOLLOWING UNLESS YOU KNOW WHAT YOU'RE DOING!! **
   CORE    = 'core-six'
   MAJOR   = '4.00'
-  MINOR   = '136'
-  BUILD   = '20140107b'
+  MINOR   = '137'
+  BUILD   = '20140108'
 
   # Internal constant used to discriminate between all the existing and
   # running versions of the AgeX framework.
