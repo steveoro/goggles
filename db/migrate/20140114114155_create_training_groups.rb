138e564a87ad1bd2c9bd3a5577fe07fc79ecb863
Stefano Alloro <steve.alloro@gmail.com>
2014-01-16 19:33:21 +0100

    update to DB structure: training groups; DB vers. 1.15.09 (not final)

--- db/migrate/20140114114155_create_training_groups.rb
+++ db/migrate/20140114114155_create_training_groups.rb
@@ -1,6 +1,10 @@
 class CreateTrainingGroups < ActiveRecord::Migration
   def change
     create_table :training_groups do |t|
+      t.integer :part_order,      :limit => 3, :null => false, :default => 0, :comment => "Overall order for this exercise part"
+      t.integer :times,           :limit => 3, :null => false, :default => 0, :comment => "Repetition number"
+      t.integer :start_and_rest,  :limit => 4, :null => false, :default => 0, :comment => "Start-rest time interval in seconds; auto-computed when left empty"
+      t.integer :pause,           :limit => 4, :null => false, :default => 0, :comment => "Rest pause interval in seconds in between repetitions"
 
       t.timestamps
     end
--- db/migrate/20140114114307_add_training_groups_to_training_rows.rb
+++ db/migrate/20140114114307_add_training_groups_to_training_rows.rb
@@ -1,4 +1,7 @@
 class AddTrainingGroupsToTrainingRows < ActiveRecord::Migration
   def change
+    change_table :training_rows do |t|
+      t.references :training_group
+    end
   end
 end
