c5c2df0f598e66daa8d4bff08db9a721542ef1d0
Stefano Alloro <steve.alloro@gmail.com>
2014-01-17 19:15:55 +0100

    experimenting with new form features; new version 4.00.144 (WIP)

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -3,8 +3,8 @@ module Version
   # ** DO NOT CHANGE THE FOLLOWING UNLESS YOU KNOW WHAT YOU'RE DOING!! **
   CORE    = 'core-six'
   MAJOR   = '4.00'
-  MINOR   = '143'
-  BUILD   = '20140116'
+  MINOR   = '144'
+  BUILD   = '20140117'
 
   # Internal constant used to discriminate between all the existing and
   # running versions of the AgeX framework.
