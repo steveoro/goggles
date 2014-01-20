b3e8b3bf8e4e2250e5c1dadde9da118a4e792cba
Stefano Alloro <steve.alloro@gmail.com>
2014-01-20 20:57:14 +0100

    added drag'n'drop ordering to training form; new vers. 4.00.144; DB struct is still WIP

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -3,8 +3,8 @@ module Version
   # ** DO NOT CHANGE THE FOLLOWING UNLESS YOU KNOW WHAT YOU'RE DOING!! **
   CORE    = 'core-six'
   MAJOR   = '4.00'
-  MINOR   = '144'
-  BUILD   = '20140117'
+  MINOR   = '145'
+  BUILD   = '20140120'
 
   # Internal constant used to discriminate between all the existing and
   # running versions of the AgeX framework.
