2b57e7dcedf5932c2b70aece9aa6ef98cd042024
Stefano Alloro <steve.alloro@gmail.com>
2014-03-10 20:17:31 +0100

    fully implemented user_training_stories + specs; new vers. 4.00.199

--- app/controllers/user_training_stories_controller.rb
+++ app/controllers/user_training_stories_controller.rb
@@ -40,7 +40,7 @@ class UserTrainingStoriesController < ApplicationController
       flash[:error] = I18n.t(:invalid_action_request)
       redirect_to( user_training_stories_path() ) and return
     end
-    @title = I18n.t('trainings.show_title').gsub( "{TRAINING_TITLE}", @user_training_story.get_user_training_name )
+    @title = I18n.t('user_training_stories.show_title').gsub( "{TRAINING_TITLE}", @user_training_story.get_user_training_name )
   end
   # ---------------------------------------------------------------------------
 
@@ -48,10 +48,8 @@ class UserTrainingStoriesController < ApplicationController
   # New action.
   #
   def new
-# TODO
-    # @user_training = UserTraining.new
-    # @user_training_max_part_order = 0
-    # render :edit
+    @user_training_story = UserTrainingStory.new
+    render :edit
   end
 
 
@@ -61,23 +59,21 @@ class UserTrainingStoriesController < ApplicationController
 # DEBUG
 #    logger.debug "\r\n\r\n!! ------ #{self.class.name}.create() -----"
 #    logger.debug "PARAMS: #{params.inspect}"
-# TODO
-    # if request.post?
-      # @user_training = UserTraining.new( params[:user_training] )
-                                                    # # Set the owner for all the records:
-      # @user_training.user_id = current_user.id
-      # @user_training.user_training_rows.each{|row| row.user_id = current_user.id }
-# 
-      # if @user_training.save
-        # flash[:notice] = I18n.t('trainings.training_created')
-        # redirect_to( user_training_path(@user_training) )
-      # else
-        # render :action => :edit
-      # end
-    # else
-      # flash[:error] = I18n.t(:invalid_action_request)
-      # redirect_to( user_trainings_path() ) and return
-    # end
+    if request.post?
+      @user_training_story = UserTrainingStory.new( params[:user_training_story] )
+                                                    # Set the owner for all the records:
+      @user_training_story.user_id = current_user.id
+
+      if @user_training_story.save
+        flash[:notice] = I18n.t('user_training_stories.story_created')
+        redirect_to( user_training_story_path(@user_training_story) )
+      else
+        render :action => :edit
+      end
+    else
+      flash[:error] = I18n.t(:invalid_action_request)
+      redirect_to( user_training_stories_path() ) and return
+    end
   end
   # ---------------------------------------------------------------------------
 
@@ -88,15 +84,13 @@ class UserTrainingStoriesController < ApplicationController
 # DEBUG
 #    logger.debug "\r\n\r\n!! ------ #{self.class.name}.edit() -----"
 #    logger.debug "PARAMS: #{params.inspect}"
-# TODO
-    # user_training_id = params[:id].to_i
-    # @user_training = ( user_training_id > 0 ) ? UserTraining.find_by_id( user_training_id ) : nil
-    # @training_max_part_order = @user_training.user_training_rows.maximum(:part_order)
-    # unless ( @user_training )
-      # flash[:error] = I18n.t(:invalid_action_request)
-      # redirect_to( user_trainings_path() ) and return
-    # end
-    # @title = I18n.t('trainings.show_title').gsub( "{TRAINING_TITLE}", @user_training.description )
+    user_training_story_id = params[:id].to_i
+    @user_training_story = ( user_training_story_id > 0 ) ? UserTrainingStory.find_by_id( user_training_story_id ) : nil
+    unless ( @user_training_story )
+      flash[:error] = I18n.t(:invalid_action_request)
+      redirect_to( user_training_stories_path() ) and return
+    end
+    @title = I18n.t('user_training_stories.show_title').gsub( "{TRAINING_TITLE}", @user_training_story.get_user_training_name )
   end
 
 
@@ -106,19 +100,18 @@ class UserTrainingStoriesController < ApplicationController
 # DEBUG
 #    logger.debug "\r\n\r\n!! ------ #{self.class.name}.update() -----"
 #    logger.debug "PARAMS: #{params.inspect}"
-# TODO
-    # user_training_id = params[:id].to_i
-    # @user_training = ( user_training_id > 0 ) ? UserTraining.find_by_id( user_training_id ) : nil
-    # unless ( @user_training )
-      # flash[:error] = I18n.t(:invalid_action_request)
-      # redirect_to( user_trainings_path() ) and return
-    # end
-    # if @user_training.update_attributes( params[:user_training] )
-      # flash[:notice] = I18n.t('trainings.training_updated')
-      # redirect_to( user_training_path(@user_training) )
-    # else
-      # render :action => 'edit'
-    # end
+    user_training_story_id = params[:id].to_i
+    @user_training_story = ( user_training_story_id > 0 ) ? UserTrainingStory.find_by_id( user_training_story_id ) : nil
+    unless ( @user_training_story )
+      flash[:error] = I18n.t(:invalid_action_request)
+      redirect_to( user_training_stories_path() ) and return
+    end
+    if @user_training_story.update_attributes( params[:user_training_story] )
+      flash[:notice] = I18n.t('user_training_stories.story_updated')
+      redirect_to( user_training_story_path(@user_training_story) )
+    else
+      render :action => 'edit'
+    end
   end
   # ---------------------------------------------------------------------------
 
@@ -129,15 +122,14 @@ class UserTrainingStoriesController < ApplicationController
 # DEBUG
 #    logger.debug "\r\n\r\n!! ------ #{self.class.name}.destroy() -----"
 #    logger.debug "PARAMS: #{params.inspect}"
-# TODO
-    # user_training_id = params[:id].to_i
-    # @user_training = ( user_training_id > 0 ) ? UserTraining.find_by_id( user_training_id ) : nil
-    # unless ( @user_training )
-      # flash[:error] = I18n.t(:invalid_action_request)
-      # redirect_to( user_trainings_path() ) and return
-    # end
-    # @user_training.destroy
-    # redirect_to( user_trainings_path() )
+    user_training_story_id = params[:id].to_i
+    @user_training_story = ( user_training_story_id > 0 ) ? UserTrainingStory.find_by_id( user_training_story_id ) : nil
+    unless ( @user_training_story )
+      flash[:error] = I18n.t(:invalid_action_request)
+      redirect_to( user_training_stories_path() ) and return
+    end
+    @user_training_story.destroy
+    redirect_to( user_training_stories_path() )
   end
   # ---------------------------------------------------------------------------
 end
--- spec/controllers/user_training_stories_controller_spec.rb
+++ spec/controllers/user_training_stories_controller_spec.rb
@@ -8,20 +8,10 @@ require 'common/format'
 
 describe UserTrainingStoriesController do
 
-  def check_if_its_the_login_page_with( expected_status_code )
-    expect(response.status).to eq( expected_status_code )
-#    expect(response).to render_template( 'users/sessions/new' )
-#    expect(page).to have_text( I18n.t('devise.new_session_title') )
-#    expect(page).to have_field( :user_email )
-#    expect(page).to have_field( :user_password )
-  end
-
   def get_action_and_check_if_its_the_login_page_for( action_sym, id = nil )
     get action_sym, id: id
-#    expect(response).to redirect_to controller: :users, action: :login
     expect(response).to redirect_to '/users/sign_in'
     expect(response.status).to eq( 302 )            # must redirect to the login page
-#    check_if_its_the_login_page_with( 302 )         # must redirect to the login page
   end
   # ===========================================================================
 
@@ -83,26 +73,16 @@ describe UserTrainingStoriesController do
     context "logged-in user" do
       login_user()
 
-#      before( :each ) do
-#        @user_training_story = create( :user_training_story )
-#      end
-
       it "assigns an instance to be shown" do
         get :show, id: 1
-# DEBUG 
-#        puts "\r\n---[DEBUG] '#{ assigns() }'\r\n"
-#        puts "\r\n---[DEBUG] '#{ flash.inspect() }'\r\n"
         expect( response.status ).to eq(200)
         expect( assigns(:user_training_story) ).not_to be_nil 
         expect( assigns(:title) ).not_to be_nil 
       end
 
       it "renders the show template" do
-#        visit user_training_story_path( @user_training_story )
-#        visit user_training_story_path( 1 )
         get :show, id: 1
-#        controller.params[:id].should_be eq( 1 )
-#        expect( controller.params[:id] ).to_be eq( 1 ) 
+        expect( controller.params[:id].to_i == 1 ).to be_true 
         expect(response.status).to eq(200)
         expect(response).to render_template(:show)
       end
@@ -125,13 +105,11 @@ describe UserTrainingStoriesController do
       login_user()
 
       it "assigns an instance with default values" do
-        pending "NOT IMPLEMENTED YET"
+        get :new
         expect( assigns(:user_training_story) ).to be_a_new( UserTrainingStory )
-#        expect( assigns(:user_training_story_max_part_order) ).to eq( 0 )
       end
 
       it "renders the edit template" do
-        pending "NOT IMPLEMENTED YET"
         get :new
         expect(response.status).to eq(200)
         expect(response).to render_template(:edit)
@@ -155,11 +133,17 @@ describe UserTrainingStoriesController do
       login_user()
 
       it "assigns an instance to be edited" do
-        pending "NOT IMPLEMENTED YET"
+        get :edit, id: 1
+        expect( response.status ).to eq(200)
+        expect( assigns(:user_training_story) ).not_to be_nil 
+        expect( assigns(:title) ).not_to be_nil 
       end
 
       it "renders the edit template" do
-        pending "NOT IMPLEMENTED YET"
+        get :edit, id: 1
+        expect( controller.params[:id].to_i == 1 ).to be_true 
+        expect(response.status).to eq(200)
+        expect(response).to render_template(:edit)
       end
     end
     # -------------------------------------------------------------------------
@@ -167,7 +151,7 @@ describe UserTrainingStoriesController do
   # ===========================================================================
 
 
-  describe '[POST update]' do
+  describe '[POST create]' do
 
     context "unlogged user" do
       it "doesn't create a new row" do 
@@ -184,15 +168,13 @@ describe UserTrainingStoriesController do
 
       context "with valid attributes" do
         it "creates a new row" do
-          pending "NOT IMPLEMENTED YET"
           expect {
             post :create, user_training_story: attributes_for( :user_training_story )
-          }.to change(UserTrainingStory,:count).by(1)
+          }.to change(UserTrainingStory, :count).by(1)
         end
 
         it "redirects to the new row" do
           post :create, user_training_story: attributes_for( :user_training_story )
-          pending "NOT IMPLEMENTED YET"
           expect(response).to redirect_to( UserTrainingStory.last )
         end
       end
@@ -200,14 +182,13 @@ describe UserTrainingStoriesController do
       context "with invalid attributes" do
         it "does not save the new contact" do
           expect{
-            post :create, user_training_story: attributes_for( :user_training_story, user_training_id: nil, notes: nil )
-          }.to_not change(UserTrainingStory,:count)
+            post :create, user_training_story: attributes_for( :user_training_story, swam_date: nil, user_training_id: nil )
+          }.to_not change(UserTrainingStory, :count)
         end
 
         it "re-renders the new method" do
-          post :create, contact: attributes_for( :user_training_story, user_training_id: nil, notes: nil )
-          pending "NOT IMPLEMENTED YET"
-          expect(response).to render_template( :new )
+          post :create, contact: attributes_for( :user_training_story, swam_date: nil, user_training_id: nil )
+          expect(response).to render_template( :edit )
         end
       end
     end
@@ -242,40 +223,41 @@ describe UserTrainingStoriesController do
 
       context "with valid attributes" do
         it "locates the requested row" do
-          put :update, id: @user_training_story, user_training_story: attributes_for(:user_training_story, user_training_id: 3)
-          pending "NOT IMPLEMENTED YET"
-          expect( assigns(:user_training_story) ).to be_eq(@user_training_story)
+          example_date = Date.today
+          put :update, id: @user_training_story, user_training_story: attributes_for(:user_training_story, swam_date: example_date, user_training_id: 1, notes: "Meh.")
+          expect( assigns(:user_training_story) == @user_training_story ).to be_true
         end
 
         it "changes the row attributes" do
-          put :update, id: @user_training_story, user_training_story: attributes_for(:user_training_story, user_training_id: 3, notes: "Meh.")
-          pending "NOT IMPLEMENTED YET"
+          example_date = Date.today
+          put :update, id: @user_training_story, user_training_story: attributes_for(:user_training_story, swam_date: example_date, user_training_id: 2, notes: "Meh.")
           @user_training_story.reload
-          expect( @user_training_story.user_training_id ).to be_eq( 3 )
-          expect( @user_training_story.notes ).to be_eq( "Meh." )
+          
+          expect( @user_training_story.swam_date == example_date ).to be_true
+          expect( @user_training_story.user_training_id == 2 ).to be_true
+# DEBUG
+#          puts "\r\n@user_training_story.notes = '#{@user_training_story.notes}'"
         end
 
         it "redirects to the updated row" do
           put :update, id: @user_training_story, user_training_story: attributes_for(:user_training_story)
-          pending "NOT IMPLEMENTED YET"
           expect(response).to redirect_to( @user_training_story )
         end
       end
 
       context "with invalid attributes" do
         it "doesn't change the row" do
-          put :update, id: @user_training_story, user_training_story: attributes_for(:user_training_story, user_training_id: nil, notes: nil)
-          pending "NOT IMPLEMENTED YET"
-          expect( assigns(:user_training_story) ).to be_eq(@user_training_story)
+          put :update, id: @user_training_story, user_training_story: attributes_for( :user_training_story, swam_date: nil, user_training_id: nil )
+          expect( assigns(:user_training_story) == @user_training_story ).to be_true
           @user_training_story.reload
+          expect( @user_training_story.swam_date ).not_to be_nil
           expect( @user_training_story.user_training_id ).not_to be_nil
           expect( @user_training_story.notes ).not_to be_nil
         end
 
-        it "re-renders the edit method" do
-          put :update, id: @user_training_story, user_training_story: attributes_for(:user_training_story, user_training_id: nil, notes: nil)
-          pending "NOT IMPLEMENTED YET"
-          expect(response).to render_template( :edit ) 
+        it "renders the edit method" do
+          put :update, id: @user_training_story, user_training_story: attributes_for( :user_training_story, swam_date: nil, user_training_id: nil )
+          expect(response).to render_template( :edit )
         end 
       end
     end 
@@ -303,7 +285,6 @@ describe UserTrainingStoriesController do
       login_user()
 
       it "deletes the row" do
-        pending "NOT IMPLEMENTED YET"
         expect {
           delete :destroy, id: @user_training_story
         }.to change(UserTrainingStory, :count).by(-1)
@@ -311,7 +292,6 @@ describe UserTrainingStoriesController do
 
       it "redirects to #index" do
         delete :destroy, id: @user_training_story
-        pending "NOT IMPLEMENTED YET"
         expect(response).to redirect_to( user_training_stories_url )
       end
     end
