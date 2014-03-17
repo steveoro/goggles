494493535e7caaab16e5b2245f0f815f81afe321
Marco Ligabue <leegaweb@gmail.com>
2014-03-17 23:23:15 +0100

    Upgrade minor version to 200

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -3,8 +3,8 @@ module Version
   # ** DO NOT CHANGE THE FOLLOWING UNLESS YOU KNOW WHAT YOU'RE DOING!! **
   CORE    = 'core-six'
   MAJOR   = '4.00'
-  MINOR   = '199'
-  BUILD   = '20140315'
+  MINOR   = '200'
+  BUILD   = '20140317'
 
   # Internal constant used to discriminate between all the existing and
   # running versions of the AgeX framework.
