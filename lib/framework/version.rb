02fb364511f10fe91e1a9e01affb6a4f178c1150
Marco Ligabue <leegaweb@gmail.com>
2014-04-02 10:08:21 +0200

    More toppings to season factory

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -3,8 +3,8 @@ module Version
   # ** DO NOT CHANGE THE FOLLOWING UNLESS YOU KNOW WHAT YOU'RE DOING!! **
   CORE    = 'core-six'
   MAJOR   = '4.00'
-  MINOR   = '209'
-  BUILD   = '20140401'
+  MINOR   = '210'
+  BUILD   = '20140402'
 
   # Internal constant used to discriminate between all the existing and
   # running versions of the AgeX framework.
--- spec/factories/seasons.rb
+++ spec/factories/seasons.rb
@@ -10,14 +10,15 @@ FactoryGirl.define do
   end
 
   factory :season_type do
-    sequence( :code ) { |n| "MAS_#(n)" }
+    #sequence( :code ) { |n| "MAS_#(n)" }
+    code { "MAS#{federation_type.code}" }
     description { "CIRCUITO REGGIANO MASTER #{federation_type.code}" }
     short_name { "MASTER #{federation_type.code}" }
     federation_type
   end
 
   factory :season do
-    sequence( :description )  { |n| "Farloque season #{n}" }
+    sequence( :description )  { |n| "Farloque season #(n)" }
     begin_date  { Date.parse("#{ 2000 + ((rand * 100) % 15).to_i }-09-01") }
     end_date    { Date.parse("#{ begin_date.year + 1 }-06-15") }
     header_year { end_date.year }
@@ -25,11 +26,12 @@ FactoryGirl.define do
   end
 
   factory :meeting do
-    sequence( :description )  { |n| "Farloque meeting #{n}" }
-    sequence( :code )  { |n| "farloque#{n}" }
-    edition {((rand * 100) % 40).to_i }
+    sequence( :description )  { |n| "Farloque meeting #(n)" }
+    sequence( :code )  { |n| "farloque#(n)" }
+    edition { ((rand * 100) % 40).to_i }
     season
-    header_year { 2000 + ((rand * 100) % 15).to_i }
+    #header_year { 2000 + ((rand * 100) % 15).to_i }
+    header_year { season.header_year }
     timing_type_id 1
     edition_type_id 1
     user_id 1
