c626e7fe740cf1ca7dc3162170906801820e3187
steveoro <steve.alloro@gmail.com>
2014-04-04 14:27:58 +0200

    updated production config to log deprecation warninings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,5 +72,5 @@ Goggles::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
+  config.active_support.deprecation = :log
 end
