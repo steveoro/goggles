c15550a5807ded49df900dd53dad87819e8bd56f
Stefano Alloro <steve.alloro@gmail.com>
2014-04-19 04:21:27 +0200

    merged with conflicting api docs subdir (now removed from repository); new vers. 4.00.233

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -3,8 +3,8 @@ module Version
   # ** DO NOT CHANGE THE FOLLOWING UNLESS YOU KNOW WHAT YOU'RE DOING!! **
   CORE    = 'core-six'
   MAJOR   = '4.00'
-  MINOR   = '231'
-  BUILD   = '20140418'
+  MINOR   = '233'
+  BUILD   = '20140419'
 
   # Internal constant used to discriminate between all the existing and
   # running versions of the AgeX framework.
