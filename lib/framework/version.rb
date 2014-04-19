235acd2e847a79237b7c8448f50a6023e714405d
Marco Ligabue <leegaweb@gmail.com>
2014-04-19 02:22:30 +0200

    Test

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -3,7 +3,7 @@ module Version
   # ** DO NOT CHANGE THE FOLLOWING UNLESS YOU KNOW WHAT YOU'RE DOING!! **
   CORE    = 'core-six'
   MAJOR   = '4.00'
-  MINOR   = '231'
+  MINOR   = '232'
   BUILD   = '20140418'
 
   # Internal constant used to discriminate between all the existing and
