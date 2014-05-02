48c220aaff9dd19af4e9aedb45c3f1654a3b3083
steveoro <steve.alloro@gmail.com>
2014-05-02 09:01:07 +0200

    better random names for user factory; new vers. 4.00.263

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,9 +5,9 @@ require 'ffaker'
 FactoryGirl.define do
 
   factory :user do
-    name                      { Faker::Name.first_name.downcase }
+    name                      { "#{Faker::Internet.user_name}-#{(rand * 1000).to_i}" }
     email                     { Faker::Internet.email }
-    description               { "#{name.camelcase} #{Faker::Name.last_name}" }
+    description               { "#{Faker::Name.first_name} #{Faker::Name.last_name}" }
     password                  "password"
     password_confirmation     "password"
     confirmed_at              { DateTime.now }
