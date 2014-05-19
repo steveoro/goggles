5a8f3c6b930d0193b30435626fe9c724990b21f2
leegaweb <leegaweb@gmail.com>
2014-05-19 22:45:50 +0200

    individual result specs first approach

--- spec/factories/swimmer.rb
+++ spec/factories/swimmer.rb
@@ -9,15 +9,30 @@ FactoryGirl.define do
     last_name                 { Faker::Name.last_name }
     gender_type_id            { (rand * 100).to_i.even? ? GenderType::FEMALE_ID : GenderType::MALE_ID }
     year_of_birth             { Date.today.year - 18 - ((rand * 100) % 60).to_i }
-
     complete_name             { "#{last_name} #{first_name}" } 
     phone_mobile              { Faker::PhoneNumber.phone_number }
     phone_number              { Faker::PhoneNumber.phone_number }
     e_mail                    { Faker::Internet.email }
     nickname                  { Faker::Internet.user_name  }
-
-#    badge # TODO
   end
   # ---------------------------------------------------------------------------
+
+  factory :team do
+    name                      { "#{city.name} swimming club ASD" }
+    editable_name             { name }
+    address                   { Faker::Address.street_address }
+    phone_mobile              { Faker::PhoneNumber.phone_number }
+    phone_number              { Faker::PhoneNumber.phone_number }
+    e_mail                    { Faker::Internet.email }   
+    city
+    user
+  end
+
+  factory :badge do
+    number                    { "#{((rand * 10).to_i)}#{((rand * 10).to_i)}#{((rand * 10).to_i)}#{((rand * 10).to_i)}#{((rand * 10).to_i)}#{((rand * 10).to_i)}#{((rand * 10).to_i)}#{((rand * 10).to_i)}" }
+    team
+    swimmer
+    user
+  end
 
 end
