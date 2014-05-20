a5690601f19691ee948d5a1404a6636141bde681
steveoro <steve.alloro@gmail.com>
2014-05-20 20:19:47 +0200

    merge commit

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -3,8 +3,8 @@ module Version
   # ** DO NOT CHANGE THE FOLLOWING UNLESS YOU KNOW WHAT YOU'RE DOING!! **
   CORE    = 'core-six'
   MAJOR   = '4.00'
-  MINOR   = '285'
-  BUILD   = '20140519'
+  MINOR   = '287'
+  BUILD   = '20140520'
 
   # Internal constant used to discriminate between all the existing and
   # running versions of the AgeX framework.
