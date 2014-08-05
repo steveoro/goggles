75461ed41f5a47d6e76ccdd8501f8967ef52c297
Marco Ligabue <leegaweb@gmail.com>
2014-08-05 22:41:23 +0200

    Removed some pendings

--- spec/controllers/swimmers_controller_spec.rb
+++ spec/controllers/swimmers_controller_spec.rb
@@ -345,13 +345,13 @@ describe SwimmersController, :type => :controller do
         it "assigns a event_type" do
           expect( assigns(:current_event) ).to be_an_instance_of( EventType )        
         end 
-        it "event type is in event_by_pool_type for current season" do
+        it "accepts only event type taht is in event_by_pool_type for current season" do
           expect( assigns(:current_event).events_by_pool_types.where(pool_type_id: @fixture_events_by_pool_type.pool_type_id).count ).to be > 0 
         end
         it "assigns timing data" do
           expect( assigns(:timing) ).to be_an_instance_of( Timing )        
         end 
-        it "timing data inserted is a valid timing" do
+        it "accepts valid timing data" do
           expect( assigns(:timing).to_hundreds ).to be > 0        
         end 
         it "assigns a positive result score" do
