12d9f9b89260dded63aa187e82eac73612a754b1
leegaweb <leegaweb@gmail.com>
2014-08-29 12:59:16 +0200

    OK. now full history by date is something concrete to watch at. Not perfect. Some cosmetics missing. Coming soon

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '4.00'
 
   # Minor version.
-  MINOR   = '454'
+  MINOR   = '456'
 
   # Current build version.
-  BUILD   = '20140828'
+  BUILD   = '20140829'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
