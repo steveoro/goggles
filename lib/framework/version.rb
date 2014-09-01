76e0a165df1db99f57b35638fe081fd7cfd4fbb7
Marco Ligabue <leegaweb@gmail.com>
2014-09-01 08:58:24 +0200

    Don't forget the version update!

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '4.00'
 
   # Minor version.
-  MINOR   = '456'
+  MINOR   = '458'
 
   # Current build version.
-  BUILD   = '20140829'
+  BUILD   = '20140831'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
