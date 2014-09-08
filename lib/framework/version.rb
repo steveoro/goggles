ee75c89fd845ded10868490902e3d73eb127f070
leegaweb <leegaweb@gmail.com>
2014-09-09 01:55:28 +0200

    Navbars and locales for medal tab. Maybe it's usable.

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '4.00'
 
   # Minor version.
-  MINOR   = '473'
+  MINOR   = '474'
 
   # Current build version.
-  BUILD   = '20140908'
+  BUILD   = '20140909'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
