e49a7bc6204832b98421eb61f0444d1c4299a4c3
leegaweb <leegaweb@gmail.com>
2014-10-03 19:32:53 +0200

    Some trials

--- app/strategies/championship_ranking_calculator.rb
+++ app/strategies/championship_ranking_calculator.rb
@@ -17,6 +17,7 @@ class ChampionshipRankingCalculator
   #
   def initialize( season )
     @season = season
+    
   end
   #-- --------------------------------------------------------------------------
   #++
@@ -31,6 +32,22 @@ class ChampionshipRankingCalculator
   # 
   def get_involved_meetings
     @involved_meetings ||= retrieve_involved_meetings
+  end
+  #-- --------------------------------------------------------------------------
+  #++
+  
+  def compute_season_ranking()
+    @championship_ranking = Hash.new
+    @championship_ranking[:columns] = ['season_individual_points', 'season_relay_points']
+    @championship_ranking[:meetings] = get_involved_meetings 
+    
+    team_ranking = []
+    get_involved_teams.each do |team|
+      team_ranking << {:team => team, :total_points => compute_season_team_points(team)}
+    end
+    @championship_ranking[:teams] = team_ranking.sort{ |p,n| p[:total_points] <=> n[:total_points] }
+    
+    @championship_ranking
   end
   #-- --------------------------------------------------------------------------
   #++
@@ -55,10 +72,17 @@ class ChampionshipRankingCalculator
   #-- --------------------------------------------------------------------------
   #++
 
-  # Computes the teams points for a given meeting 
+  # Computes the teams points for a given team 
   #
-  def retreive_season_team_points
-    @season.meeting_team_scores.has_season_points
+  def compute_season_team_points(team)
+    # Sum team points among season meetings
+    @season.meeting_team_scores.has_season_points.for_team(team).select('(sum(season_team_points) + sum(season_individual_points) + sum(season_relay_points)) as total_pts').first.total_pts.to_i
+  end
+
+  # Retrievs the teams points for teh season meetings 
+  #
+  def retreive_season_points
+    @season.meeting_team_scores.has_season_points.select('team_id, meeting_id, season_team_points, season_individual_points, season_relay_points')
   end
   #-- --------------------------------------------------------------------------
   #++
