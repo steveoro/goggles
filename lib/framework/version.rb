380f5361cbc8efff4e00880c77179d8a9571c530
leegaweb <leegaweb@gmail.com>
2014-10-12 22:34:03 +0200

    add multiple seasons calculation

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '4.00'
 
   # Minor version.
-  MINOR   = '558'
+  MINOR   = '560'
 
   # Current build version.
-  BUILD   = '20141010'
+  BUILD   = '20141012'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
