fbea7f97d674be0baf6b0cb3c7a8a592c40d0e70
Marco Ligabue <leegaweb@gmail.com>
2014-11-29 11:30:18 +0100

    Fix wrong swimmers data

---
+++ db/diff.new/201411291120all_fixup_wrong_swimmers_data.sql
@@ -0,0 +1,3 @@
+update swimmers s set s.last_name = 'PEDERZANI', s.complete_name = 'PEDERZANI GIULIA' where s.id = 1848;
+update swimmers s set s.first_name = 'MIRKO', s.complete_name = 'LASI MIRKO' where s.id = 1060;
+update swimmers s set s.year_of_birth = 1968 where s.id = 1808;
