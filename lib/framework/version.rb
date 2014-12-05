fb7848a5e8340474affcbe1517094097362ffcf0
leegaweb <leegaweb@gmail.com>
2014-12-05 09:28:33 +0100

    And the version update?!?

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '4.00'
 
   # Minor version.
-  MINOR   = '659'
+  MINOR   = '660'
 
   # Current build version.
-  BUILD   = '20141204'
+  BUILD   = '20141205'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
