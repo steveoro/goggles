f13502df3b193349ebba4ba40f7e8ecab58a0564
Marco Ligabue <leegaweb@gmail.com>
2014-12-09 23:17:01 +0100

    One more spec

--- spec/helpers/swimmers_spec.rb
+++ spec/helpers/swimmers_spec.rb
@@ -233,4 +233,22 @@ describe SwimmersHelper, :type => :helper do
   # ---------------------------------------------------------------------------
   # (logged-in user END)
   # ---------------------------------------------------------------------------
+
+
+  describe "#cache_key_for_swimmer" do
+    let(:result_plain) { helper.send(:cache_key_for_swimmer, 'radio', (rand * 10000).to_i, DateTime.now ) }
+
+    it "returns a String" do
+      expect( result_plain ).to be_an_instance_of(String)
+    end
+    it "returns a non empty string" do
+      expect( result_plain.size ).to be >= 6
+    end
+    it "contains as many numbers as there are IDs in the collection" do
+      # 2 IDs + 1 locale key + 2 flag + 1 date
+      expect( result_plain.split('-').count ).to eq( 6 )
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
 end
