264208a88ff530bfc5a1dc142472eeb5ec0a8284
steveoro <steve.alloro@gmail.com>
2014-12-22 21:21:34 +0100

    diff applied only on dev recovery dump

--- db/diff.new/201412220900prod_fin_badges_fix.sql
+++ db/diff.new/201412220900prod_fin_badges_fix.sql
@@ -8,7 +8,7 @@ START TRANSACTION;
 -- Fix null team affiliation in badges
 update badges b
 set b.team_affiliation_id = (select ta.id from team_affiliations ta where ta.team_id = b.team_id and ta.season_id = b.season_id)
-where b.team_affiliation_id is null
+where b.team_affiliation_id is null;
 
 COMMIT;
 -- Fine script
