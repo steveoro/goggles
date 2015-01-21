97e3e416915efbbaffd8124ee3692476dc67b41d
steveoro <steve.alloro@gmail.com>
2015-01-21 11:14:28 +0100

    added age display in swimmer analysis report during data-import phase 1.1

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '4.00'
 
   # Minor version.
-  MINOR   = '709'
+  MINOR   = '711'
 
   # Current build version.
-  BUILD   = '20150120'
+  BUILD   = '20150121'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
