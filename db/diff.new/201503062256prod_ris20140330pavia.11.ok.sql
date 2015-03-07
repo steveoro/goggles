809dbeb1057e379e1996f12a038b7b2238836f90
steveoro <steve.alloro@gmail.com>
2015-03-07 01:12:38 +0100

    data-import: added FIN result for pavia, season 132; dev dump updated

---
+++ db/diff.new/201503062256prod_ris20140330pavia.11.ok.sql
@@ -0,0 +1,11 @@
+--
+-- *** Suggested SQL actions: ***
+--
+
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
+
+-- Processing:...'DEL VAN STEFANIA' (1988, gender: 2)
+
+COMMIT;
---
+++ db/diff.new/201503062256prod_ris20140330pavia.12.diff.sql
@@ -0,0 +1,6 @@
+-- *** SQL Diff file for ris20140330pavia.txt ***
+-- Timestamp: 201503062256
+UPDATE `meetings` SET `notes`='30 Marzo 2014\r\nAICS Pavia Nuoto' WHERE (`id`=13295);
+
+
+-- Last completed phase code: 12
