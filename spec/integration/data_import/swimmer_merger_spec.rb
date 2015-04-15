bbe279e91826cddfd3fd07cd1acdecfcaddd461f
steveoro <steve.alloro@gmail.com>
2015-04-15 13:35:22 +0200

    swimmer merger skeleton class

---
+++ spec/integration/data_import/swimmer_merger_spec.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+require 'spec_helper'
+
+# [Steve, 20140925] we must use a relative path for sake of CI server happyness:
+require_relative '../../../lib/data_import/services/swimmer_merger'
+
+
+describe SwimmerMerger, type: :integration do
+
+  describe "when merging swimmers having NO badge duplicates, NO dup affiliation (but randomly chosen)," do
+    # TODO
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+
+  describe "when merging swimmers having 3 source non-duplicate badges & dup affiliation," do
+    # TODO
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+
+  describe "when merging swimmers having >1 dups, 3 non-dups source badges & dup affiliation," do
+    # TODO
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+end
