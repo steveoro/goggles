1c223cc3bfdb586f8572ba7c33db0f1396bbd4e5
Marco Ligabue <marco@linux-87m5.homenet.telecomitalia.it>
2015-11-30 22:14:50 +0100

    Team scores

---
+++ db/diff.new/201511302050prod_ris20151129csiprova1.12.diff.sql
@@ -0,0 +1,7 @@
+-- *** SQL Diff file for ris20151129csiprova1.csv ***
+-- Timestamp: 201511302050
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1596, NULL, 'GRUPPO SPORTIVO RIALE', 0, 290, 151, 1, '2015-11-30 20:50:12', '2015-11-30 20:50:12', 1);
+
+
+-- Last completed phase code: 12
