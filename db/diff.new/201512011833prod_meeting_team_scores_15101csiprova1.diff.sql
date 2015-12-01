2bfbd4049c7812ce5e6cd5602e367024958e744d
Marco Ligabue <marco@linux-87m5.homenet.telecomitalia.it>
2015-12-01 18:42:43 +0100

    50 special, some fix

---
+++ db/diff.new/201512011833prod_meeting_team_scores_15101csiprova1.diff.sql
@@ -0,0 +1,51 @@
+--
+-- Team scores calculation for Meeting 1A PROVA REGIONALE CSI Circuito regionale Emilia master CSI 2015/2016
+-- 01 December 2015 18:33:25
+-- Begin script
+--
+
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=618.0, `sum_relay_points`=72.0, `sum_team_points`=13.0, `meeting_individual_points`=618.0, `meeting_relay_points`=72.0, `meeting_team_points`=13.0, `season_individual_points`=618.0, `season_relay_points`=72.0, `season_team_points`=13.0
+  WHERE (`id`=8784);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=934.0, `sum_relay_points`=114.0, `sum_team_points`=0.0, `meeting_individual_points`=934.0, `meeting_relay_points`=114.0, `meeting_team_points`=0.0, `season_individual_points`=934.0, `season_relay_points`=114.0, `season_team_points`=0.0
+  WHERE (`id`=8785);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=448.0, `sum_relay_points`=51.0, `sum_team_points`=14.0, `meeting_individual_points`=448.0, `meeting_relay_points`=51.0, `meeting_team_points`=14.0, `season_individual_points`=448.0, `season_relay_points`=51.0, `season_team_points`=14.0
+  WHERE (`id`=8786);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=268.0, `sum_relay_points`=23.0, `sum_team_points`=39.0, `meeting_individual_points`=268.0, `meeting_relay_points`=23.0, `meeting_team_points`=39.0, `season_individual_points`=268.0, `season_relay_points`=23.0, `season_team_points`=39.0
+  WHERE (`id`=8787);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=594.0, `sum_relay_points`=108.0, `sum_team_points`=17.0, `meeting_individual_points`=594.0, `meeting_relay_points`=108.0, `meeting_team_points`=17.0, `season_individual_points`=594.0, `season_relay_points`=108.0, `season_team_points`=17.0
+  WHERE (`id`=8788);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=42.0, `sum_relay_points`=0.0, `sum_team_points`=52.0, `meeting_individual_points`=42.0, `meeting_relay_points`=0.0, `meeting_team_points`=52.0, `season_individual_points`=42.0, `season_relay_points`=0.0, `season_team_points`=52.0
+  WHERE (`id`=8789);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=201.0, `sum_relay_points`=26.0, `sum_team_points`=45.0, `meeting_individual_points`=201.0, `meeting_relay_points`=26.0, `meeting_team_points`=45.0, `season_individual_points`=201.0, `season_relay_points`=26.0, `season_team_points`=45.0
+  WHERE (`id`=8790);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=228.0, `sum_relay_points`=36.0, `sum_team_points`=41.0, `meeting_individual_points`=228.0, `meeting_relay_points`=36.0, `meeting_team_points`=41.0, `season_individual_points`=228.0, `season_relay_points`=36.0, `season_team_points`=41.0
+  WHERE (`id`=8791);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=600.0, `sum_relay_points`=78.0, `sum_team_points`=14.0, `meeting_individual_points`=600.0, `meeting_relay_points`=78.0, `meeting_team_points`=14.0, `season_individual_points`=600.0, `season_relay_points`=78.0, `season_team_points`=14.0
+  WHERE (`id`=8792);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=25.0, `sum_relay_points`=0.0, `sum_team_points`=52.0, `meeting_individual_points`=25.0, `meeting_relay_points`=0.0, `meeting_team_points`=52.0, `season_individual_points`=25.0, `season_relay_points`=0.0, `season_team_points`=52.0
+  WHERE (`id`=8793);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=216.0, `sum_relay_points`=9.0, `sum_team_points`=41.0, `meeting_individual_points`=216.0, `meeting_relay_points`=9.0, `meeting_team_points`=41.0, `season_individual_points`=216.0, `season_relay_points`=9.0, `season_team_points`=41.0
+  WHERE (`id`=8794);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=138.0, `sum_relay_points`=18.0, `sum_team_points`=42.0, `meeting_individual_points`=138.0, `meeting_relay_points`=18.0, `meeting_team_points`=42.0, `season_individual_points`=138.0, `season_relay_points`=18.0, `season_team_points`=42.0
+  WHERE (`id`=8795);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=56.0, `sum_relay_points`=0.0, `sum_team_points`=47.0, `meeting_individual_points`=56.0, `meeting_relay_points`=0.0, `meeting_team_points`=47.0, `season_individual_points`=56.0, `season_relay_points`=0.0, `season_team_points`=47.0
+  WHERE (`id`=8796);
+UPDATE `meeting_team_scores`
+  SET `sum_individual_points`=24.0, `sum_relay_points`=0.0, `sum_team_points`=54.0, `meeting_individual_points`=24.0, `meeting_relay_points`=0.0, `meeting_team_points`=54.0, `season_individual_points`=24.0, `season_relay_points`=0.0, `season_team_points`=54.0
+  WHERE (`id`=8797);
+
+-- Team scores calculation for Meeting 1A PROVA REGIONALE CSI Circuito regionale Emilia master CSI 2015/2016 done
+-- Script ended
