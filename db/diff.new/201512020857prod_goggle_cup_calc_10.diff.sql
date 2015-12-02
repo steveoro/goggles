e36f471a1bb10580d7c68da8fdeccadd73bacaf5
Marco Ligabue <marco@linux-87m5.homenet.telecomitalia.it>
2015-12-02 09:01:02 +0100

    Fix female 50 special

---
+++ db/diff.new/201512020857prod_goggle_cup_calc_10.diff.sql
@@ -0,0 +1,11 @@
+-- GoggleCup: Ober Cup 2016
+
+-- Meeting  : 1A PROVA REGIONALE CSI Circuito regionale Emilia master CSI 2015/2016
+-- PESARE REBECCA 50DO 36"70: 1000.0 (36"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=255473);
+-- ZULIANI MATTEO 50DO 30"80: 977.27 (30"10)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=977.27
+  WHERE (`id`=255483);
