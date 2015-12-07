5de36da5fba00a7866dcef371dfe056a800a4f14
Marco Ligabue <marco@linux-87m5.homenet.telecomitalia.it>
2015-12-07 21:39:02 +0100

    Riccione 2015 import

---
+++ db/diff.new/201512071824prod_ris20151205riccione.11.ok.sql
@@ -0,0 +1,162 @@
+--
+-- *** Suggested SQL actions: ***
+--
+
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
+
+-- Processing:...'Adriakos Nuoto - Senigall':
+-- aliased with: 'ADRIAKOS NM SENIGAL' (ID:130)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (426, '2015-12-07 18:39:34', '2015-12-07 18:39:34', 'Adriakos Nuoto - Senigall', 130);
+
+
+-- Processing:...'Amat.Nuoto Lib. Soc.Coop':
+-- aliased with: 'AMATORI NUOTO LIB' (ID:294)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (427, '2015-12-07 18:39:34', '2015-12-07 18:39:34', 'Amat.Nuoto Lib. Soc.Coop', 294);
+
+
+-- Processing:...'Amatori N Casale di Scodo':
+-- aliased with: 'AM.NUOTO CASALE SCO' (ID:293)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (428, '2015-12-07 18:39:34', '2015-12-07 18:39:34', 'Amatori N Casale di Scodo', 293);
+
+
+-- Processing:...'AVEZZANO NUOTO S.S.DIL.':
+
+-- Processing:...'CUS Bologna':
+-- aliased with: 'C.U.S. BOLOGNA' (ID:253)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (429, '2015-12-07 18:39:35', '2015-12-07 18:39:35', 'CUS Bologna', 253);
+
+
+-- Processing:...'DDS ssd':
+-- aliased with: 'DDS' (ID:50)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (430, '2015-12-07 18:39:35', '2015-12-07 18:39:35', 'DDS ssd', 50);
+
+
+-- Processing:...'Dimensione N Pontedera':
+-- aliased with: 'D.N. PONTEDERA' (ID:111)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (431, '2015-12-07 18:39:35', '2015-12-07 18:39:35', 'Dimensione N Pontedera', 111);
+
+
+-- Processing:...'Fanum Fortunae Nuoto':
+-- aliased with: 'FANUM FORTUNAE ASD' (ID:137)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (432, '2015-12-07 18:39:35', '2015-12-07 18:39:35', 'Fanum Fortunae Nuoto', 137);
+
+
+-- Processing:...'Il Grillo ssd  Civitanova':
+-- aliased with: 'IL GRILLO' (ID:127)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (433, '2015-12-07 18:39:35', '2015-12-07 18:39:35', 'Il Grillo ssd  Civitanova', 127);
+
+
+-- Processing:...'Interamnia Fitness asd':
+
+-- Processing:...'Nuoto Cl.Azzurra 91 - BO':
+-- aliased with: 'N.C. AZZURRA 1991' (ID:9)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (434, '2015-12-07 18:39:35', '2015-12-07 18:39:35', 'Nuoto Cl.Azzurra 91 - BO', 9);
+
+
+-- Processing:...'Nuoto Cl.Azzurra 91 - BO - B':
+-- aliased with: 'N.C. AZZURRA 1991' (ID:9)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (435, '2015-12-07 18:39:35', '2015-12-07 18:39:35', 'Nuoto Cl.Azzurra 91 - BO - B', 9);
+
+
+-- Processing:...'Olimpic Swim Pro sd - Vil':
+-- aliased with: 'OLIMPIC SWIM PRO' (ID:356)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (436, '2015-12-07 18:39:35', '2015-12-07 18:39:35', 'Olimpic Swim Pro sd - Vil', 356);
+
+
+-- Processing:...'Pegognaga Nuoto':
+-- aliased with: 'PEGOGNAGA N LIB' (ID:379)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (437, '2015-12-07 18:39:35', '2015-12-07 18:39:35', 'Pegognaga Nuoto', 379);
+
+
+-- Processing:...'PISCINE DELLO STADIO SSD ARL':
+
+-- Processing:...'Pol Pian del Bruscolo':
+-- aliased with: 'POLISP. PIAN DEL BR' (ID:380)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (438, '2015-12-07 18:39:36', '2015-12-07 18:39:36', 'Pol Pian del Bruscolo', 380);
+
+
+-- Processing:...'Pol.Bondeno Chi Gioca':
+
+-- Processing:...'Polisport srl - C.Castell':
+-- aliased with: 'POLISPORT SRL' (ID:117)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (439, '2015-12-07 18:39:36', '2015-12-07 18:39:36', 'Polisport srl - C.Castell', 117);
+
+
+-- Processing:...'RUSSIAN MASTERS-ALL STARS':
+
+-- Processing:...'Sisport Spa ssd':
+-- aliased with: 'SISPORT FIAT S.P.A.' (ID:226)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (440, '2015-12-07 18:39:37', '2015-12-07 18:39:37', 'Sisport Spa ssd', 226);
+
+
+-- Processing:...'ssd Stilelibero - Preganz':
+-- aliased with: 'SSD STILE LIBERO SRL' (ID:179)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (441, '2015-12-07 18:39:37', '2015-12-07 18:39:37', 'ssd Stilelibero - Preganz', 179);
+
+
+-- Processing:...'Team Futura asd - Emilia':
+
+-- Processing:...'Teramo Nuoto':
+-- aliased with: 'TERAMO NUOTO ASD' (ID:757)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (442, '2015-12-07 18:39:37', '2015-12-07 18:39:37', 'Teramo Nuoto', 757);
+
+
+-- Processing:...'Tibidabo ssd - Concesio':
+-- aliased with: 'TIBIDABO SSD' (ID:97)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (443, '2015-12-07 18:39:37', '2015-12-07 18:39:37', 'Tibidabo ssd - Concesio', 97);
+
+
+-- Processing:...'Usd Nuoto Montefeltro':
+-- aliased with: 'NUOTO MONTEFELTRO' (ID:136)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (444, '2015-12-07 18:39:37', '2015-12-07 18:39:37', 'Usd Nuoto Montefeltro', 136);
+
+
+COMMIT;
+--
+-- *** Suggested SQL actions: ***
+--
+
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
+
+-- Processing:...'BOGA RAFFAELLA' (1972, gender: 2)
+
+-- Processing:...'CASADEI NICOLA' (1979, gender: 1)
+
+-- Processing:...'GAVINA GLAUCO' (1970, gender: 1)
+-- aliased with: 'CAVINA GLAUCO' (ID:19010)
+INSERT INTO `data_import_swimmer_aliases` (`id`, `created_at`, `updated_at`, `complete_name`, `swimmer_id`)
+  VALUES (275, '2015-12-07 18:46:45', '2015-12-07 18:46:45', 'GAVINA GLAUCO', 19010);
+
+
+-- Processing:...'IAVARONE FEDERICA' (1990, gender: 2)
+
+-- Processing:...'NASINI MIRCO' (1965, gender: 1)
+-- aliased with: 'NASINI MIRKO' (ID:6947)
+INSERT INTO `data_import_swimmer_aliases` (`id`, `created_at`, `updated_at`, `complete_name`, `swimmer_id`)
+  VALUES (276, '2015-12-07 18:46:46', '2015-12-07 18:46:46', 'NASINI MIRCO', 6947);
+
+
+COMMIT;
---
+++ db/diff.new/201512071824prod_ris20151205riccione.12.diff.sql
@@ -0,0 +1,142 @@
+-- *** SQL Diff file for ris20151205riccione.txt ***
+-- Timestamp: 201512071824
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1654, NULL, 'ADRIAKOS NM SENIGAL', 0, 130, 152, 1, '2015-12-07 18:46:49', '2015-12-07 18:46:49', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1655, NULL, 'AMATORI NUOTO LIB', 0, 294, 152, 1, '2015-12-07 18:46:49', '2015-12-07 18:46:49', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1656, NULL, 'AM.NUOTO CASALE SCO', 0, 293, 152, 1, '2015-12-07 18:46:49', '2015-12-07 18:46:49', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1657, NULL, 'DDS', 0, 50, 152, 1, '2015-12-07 18:46:51', '2015-12-07 18:46:51', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1658, NULL, 'D.N. PONTEDERA', 0, 111, 152, 1, '2015-12-07 18:46:52', '2015-12-07 18:46:52', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1659, NULL, 'FANUM FORTUNAE ASD', 0, 137, 152, 1, '2015-12-07 18:46:52', '2015-12-07 18:46:52', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1660, NULL, 'IL GRILLO', 0, 127, 152, 1, '2015-12-07 18:46:53', '2015-12-07 18:46:53', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1661, NULL, 'N.C. AZZURRA 1991', 0, 9, 152, 1, '2015-12-07 18:46:54', '2015-12-07 18:46:54', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1662, NULL, 'OLIMPIC SWIM PRO', 0, 356, 152, 1, '2015-12-07 18:46:55', '2015-12-07 18:46:55', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1663, NULL, 'PEGOGNAGA N LIB', 0, 379, 152, 1, '2015-12-07 18:46:55', '2015-12-07 18:46:55', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1664, NULL, 'POLISP. PIAN DEL BR', 0, 380, 152, 1, '2015-12-07 18:46:55', '2015-12-07 18:46:55', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1665, NULL, 'POLISPORT SRL', 0, 117, 152, 1, '2015-12-07 18:46:55', '2015-12-07 18:46:55', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1666, NULL, 'SISPORT FIAT S.P.A.', 0, 226, 152, 1, '2015-12-07 18:46:57', '2015-12-07 18:46:57', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1667, NULL, 'TERAMO NUOTO ASD', 0, 757, 152, 1, '2015-12-07 18:46:58', '2015-12-07 18:46:58', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1668, NULL, 'TIBIDABO SSD', 0, 97, 152, 1, '2015-12-07 18:46:58', '2015-12-07 18:46:58', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1669, NULL, 'NUOTO MONTEFELTRO', 0, 136, 152, 1, '2015-12-07 18:46:59', '2015-12-07 18:46:59', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1670, NULL, 'SSD STILE LIBERO SRL', 0, 179, 152, 1, '2015-12-07 18:47:00', '2015-12-07 18:47:00', 1);
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5575, 1, 56, 22, 152, 3, 2, 33, 926, '2015-12-07 18:56:41', '2015-12-07 18:56:41');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5576, 1, 54, 82, 152, 3, 2, 33, 927, '2015-12-07 18:56:42', '2015-12-07 18:56:42');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5577, 1, 57, 82, 152, 3, 2, 33, 928, '2015-12-07 18:56:46', '2015-12-07 18:56:46');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5578, 2, 5, 18, 152, 3, 2, 33, 929, '2015-12-07 18:56:48', '2015-12-07 18:56:48');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5579, 2, 16, 58, 152, 3, 2, 33, 930, '2015-12-07 18:56:49', '2015-12-07 18:56:49');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5580, 1, 43, 95, 152, 3, 2, 32, 926, '2015-12-07 18:56:50', '2015-12-07 18:56:50');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5581, 1, 43, 48, 152, 3, 2, 32, 927, '2015-12-07 18:56:51', '2015-12-07 18:56:51');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5582, 1, 45, 9, 152, 3, 2, 32, 928, '2015-12-07 18:56:53', '2015-12-07 18:56:53');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5583, 1, 49, 39, 152, 3, 2, 32, 929, '2015-12-07 18:56:55', '2015-12-07 18:56:55');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5584, 1, 59, 30, 152, 3, 2, 32, 930, '2015-12-07 18:56:56', '2015-12-07 18:56:56');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5585, 2, 5, 70, 152, 2, 2, 26, 926, '2015-12-07 18:56:57', '2015-12-07 18:56:57');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5586, 2, 6, 45, 152, 2, 2, 26, 927, '2015-12-07 18:56:57', '2015-12-07 18:56:57');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5587, 2, 7, 92, 152, 2, 2, 26, 928, '2015-12-07 18:56:58', '2015-12-07 18:56:58');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5588, 2, 18, 61, 152, 2, 2, 26, 929, '2015-12-07 18:56:59', '2015-12-07 18:56:59');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5589, 1, 47, 91, 152, 1, 2, 26, 926, '2015-12-07 18:57:00', '2015-12-07 18:57:00');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5590, 1, 46, 52, 152, 1, 2, 26, 927, '2015-12-07 18:57:00', '2015-12-07 18:57:00');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5591, 1, 48, 97, 152, 1, 2, 26, 928, '2015-12-07 18:57:01', '2015-12-07 18:57:01');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5592, 1, 56, 3, 152, 1, 2, 26, 929, '2015-12-07 18:57:03', '2015-12-07 18:57:03');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5593, 2, 5, 0, 152, 1, 2, 26, 930, '2015-12-07 18:57:03', '2015-12-07 18:57:03');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5594, 2, 20, 28, 152, 1, 2, 26, 931, '2015-12-07 18:57:04', '2015-12-07 18:57:04');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5595, 1, 54, 5, 152, 2, 2, 25, 926, '2015-12-07 18:57:05', '2015-12-07 18:57:05');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5596, 1, 51, 86, 152, 2, 2, 25, 927, '2015-12-07 18:57:05', '2015-12-07 18:57:05');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5597, 1, 54, 40, 152, 2, 2, 25, 928, '2015-12-07 18:57:06', '2015-12-07 18:57:06');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5598, 2, 1, 34, 152, 2, 2, 25, 929, '2015-12-07 18:57:06', '2015-12-07 18:57:06');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5599, 1, 36, 12, 152, 1, 2, 25, 926, '2015-12-07 18:57:07', '2015-12-07 18:57:07');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5600, 1, 35, 94, 152, 1, 2, 25, 927, '2015-12-07 18:57:08', '2015-12-07 18:57:08');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5601, 1, 38, 31, 152, 1, 2, 25, 928, '2015-12-07 18:57:10', '2015-12-07 18:57:10');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5602, 1, 43, 49, 152, 1, 2, 25, 929, '2015-12-07 18:57:11', '2015-12-07 18:57:11');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (5603, 1, 50, 54, 152, 1, 2, 25, 930, '2015-12-07 18:57:11', '2015-12-07 18:57:11');
+
+
+-- Last completed phase code: 12
