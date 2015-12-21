9efe85770685dc7966f15d29363d8e596ee16257
Marco Ligabue <marco@leega-linux.homenet.telecomitalia.it>
2015-12-22 00:44:27 +0100

    Parma Coopernuoto results

---
+++ db/diff.new/201512212302prod_ris20151220parmacoopernuoto.11.ok.sql
@@ -0,0 +1,184 @@
+--
+-- *** Suggested SQL actions: ***
+--
+
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
+
+-- Processing:...'Acquaviva 2001 ssd - egna':
+-- aliased with: 'ACQUAVIVA 2001' (ID:12)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (445, '2015-12-21 23:17:22', '2015-12-21 23:17:22', 'Acquaviva 2001 ssd - egna', 12);
+
+
+-- Processing:...'Activa Nuoto asd - Piacen':
+-- aliased with: 'ACTIVA NUOTO - PIAC' (ID:567)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (446, '2015-12-21 23:17:22', '2015-12-21 23:17:22', 'Activa Nuoto asd - Piacen', 567);
+
+
+-- Processing:...'ASA asd - Cinisello Balsa':
+-- aliased with: 'ASA  CINISELLO' (ID:77)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (447, '2015-12-21 23:17:22', '2015-12-21 23:17:22', 'ASA asd - Cinisello Balsa', 77);
+
+
+-- Processing:...'asd Quanta Sp.Village - M':
+-- aliased with: 'QUANTA SPORT VILLAG' (ID:332)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (448, '2015-12-21 23:17:22', '2015-12-21 23:17:22', 'asd Quanta Sp.Village - M', 332);
+
+
+-- Processing:...'BPM PN Sport Management':
+-- aliased with: 'SPORT MANAGEMENT' (ID:71)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (449, '2015-12-21 23:17:22', '2015-12-21 23:17:22', 'BPM PN Sport Management', 71);
+
+
+-- Processing:...'Can. Vittorino da Feltre':
+-- aliased with: 'CAN VITTORINO' (ID:100)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (450, '2015-12-21 23:17:22', '2015-12-21 23:17:22', 'Can. Vittorino da Feltre', 100);
+
+
+-- Processing:...'Canottieri Nino Bixio':
+-- aliased with: 'CAN NINO BIXIO' (ID:686)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (451, '2015-12-21 23:17:22', '2015-12-21 23:17:22', 'Canottieri Nino Bixio', 686);
+
+
+-- Processing:...'Cilo ssd - Milano':
+-- aliased with: 'CILO SRL SSD' (ID:231)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (452, '2015-12-21 23:17:22', '2015-12-21 23:17:22', 'Cilo ssd - Milano', 231);
+
+
+-- Processing:...'Coopernuoto - A':
+-- aliased with: 'COOPERNUOTO' (ID:42)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (453, '2015-12-21 23:17:22', '2015-12-21 23:17:22', 'Coopernuoto - A', 42);
+
+
+-- Processing:...'Coopernuoto - B':
+-- aliased with: 'COOPERNUOTO' (ID:42)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (454, '2015-12-21 23:17:22', '2015-12-21 23:17:22', 'Coopernuoto - B', 42);
+
+
+-- Processing:...'Errenuotofidenza':
+
+-- Processing:...'Fiorenzuola Nuoto 90':
+-- aliased with: 'FIORENZUOLA N 90' (ID:754)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (455, '2015-12-21 23:17:23', '2015-12-21 23:17:23', 'Fiorenzuola Nuoto 90', 754);
+
+
+-- Processing:...'Forum ssd - Assago':
+-- aliased with: 'FORUM SSD' (ID:96)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (456, '2015-12-21 23:17:23', '2015-12-21 23:17:23', 'Forum ssd - Assago', 96);
+
+
+-- Processing:...'Gestisport Coop ssd - Car':
+-- aliased with: 'GESTISPORT COOP' (ID:61)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (457, '2015-12-21 23:17:23', '2015-12-21 23:17:23', 'Gestisport Coop ssd - Car', 61);
+
+
+-- Processing:...'Lerici Sport ssd':
+-- aliased with: 'LERICI SPORT 1954' (ID:396)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (458, '2015-12-21 23:17:23', '2015-12-21 23:17:23', 'Lerici Sport ssd', 396);
+
+
+-- Processing:...'Master Melzo Nuoto - A':
+-- aliased with: 'MASTER MELZO N' (ID:64)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (459, '2015-12-21 23:17:23', '2015-12-21 23:17:23', 'Master Melzo Nuoto - A', 64);
+
+
+-- Processing:...'Master Melzo Nuoto - B':
+-- aliased with: 'MASTER MELZO N' (ID:64)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (460, '2015-12-21 23:17:23', '2015-12-21 23:17:23', 'Master Melzo Nuoto - B', 64);
+
+
+-- Processing:...'Mioclub ssd':
+
+-- Processing:...'Nuoto Sud Milano - Pieve':
+-- aliased with: 'NUOTO SUD MILANO' (ID:748)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (461, '2015-12-21 23:17:23', '2015-12-21 23:17:23', 'Nuoto Sud Milano - Pieve', 748);
+
+
+-- Processing:...'Rari Nantes Saronno':
+-- aliased with: 'RN SARONNO' (ID:63)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (462, '2015-12-21 23:17:24', '2015-12-21 23:17:24', 'Rari Nantes Saronno', 63);
+
+
+-- Processing:...'Sky Line Nuoto - C.puster':
+-- aliased with: 'SKY LINE N' (ID:98)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (463, '2015-12-21 23:17:24', '2015-12-21 23:17:24', 'Sky Line Nuoto - C.puster', 98);
+
+
+-- Processing:...'Tecri Nuoto - Cornaredo':
+-- aliased with: 'TECRI NUOTO ASD' (ID:69)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (464, '2015-12-21 23:17:24', '2015-12-21 23:17:24', 'Tecri Nuoto - Cornaredo', 69);
+
+
+-- Processing:...'Villa Bonelli Nuoto asd - A':
+-- aliased with: 'VILLA BONELLI NUOTO' (ID:79)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (465, '2015-12-21 23:17:24', '2015-12-21 23:17:24', 'Villa Bonelli Nuoto asd - A', 79);
+
+
+-- Processing:...'Villa Bonelli Nuoto asd - B':
+-- aliased with: 'VILLA BONELLI NUOTO' (ID:79)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (466, '2015-12-21 23:17:24', '2015-12-21 23:17:24', 'Villa Bonelli Nuoto asd - B', 79);
+
+
+COMMIT;
+--
+-- *** Suggested SQL actions: ***
+--
+
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
+
+-- Processing:...'GIUBBOLINI ALESSANDRO' (1972, gender: 1)
+
+-- Processing:...'JOTTINI STEFANO' (1982, gender: 1)
+
+-- Processing:...'NOBILI CESARE BRENNO' (1971, gender: 1)
+-- aliased with: 'NOBILI CESARE' (ID:1514)
+INSERT INTO `data_import_swimmer_aliases` (`id`, `created_at`, `updated_at`, `complete_name`, `swimmer_id`)
+  VALUES (277, '2015-12-21 23:21:37', '2015-12-21 23:21:37', 'NOBILI CESARE BRENNO', 1514);
+
+
+-- Processing:...'PARDINI LORENZO' (1988, gender: 1)
+
+-- Processing:...'PASCON DEBORA' (1990, gender: 2)
+
+-- Processing:...'PIETTA PAOLO' (1974, gender: 1)
+
+-- Processing:...'ROSATI MASSIMO' (1959, gender: 1)
+
+-- Processing:...'SCIACCA NICOLA MARCO' (1979, gender: 1)
+-- aliased with: 'SCIACCA NICOLA' (ID:10551)
+INSERT INTO `data_import_swimmer_aliases` (`id`, `created_at`, `updated_at`, `complete_name`, `swimmer_id`)
+  VALUES (278, '2015-12-21 23:21:38', '2015-12-21 23:21:38', 'SCIACCA NICOLA MARCO', 10551);
+
+
+-- Processing:...'VENE` FRANCESCO' (1973, gender: 1)
+-- aliased with: 'VENÈ FRANCESCO' (ID:3949)
+INSERT INTO `data_import_swimmer_aliases` (`id`, `created_at`, `updated_at`, `complete_name`, `swimmer_id`)
+  VALUES (279, '2015-12-21 23:21:38', '2015-12-21 23:21:38', 'VENE` FRANCESCO', 3949);
+
+
+COMMIT;
---
+++ db/diff.new/201512220035prod_goggle_cup_calc_10.diff.sql
@@ -0,0 +1,207 @@
+-- GoggleCup: Ober Cup 2016
+
+-- Meeting  : IV TROFEO COOPERNUOTO
+-- BUDASSI VALENTINA 400SL 5'19"08: 1067.29 (5'40"55)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1067.29
+  WHERE (`id`=257836);
+-- CANTONI SAMANTA 400SL 6'27"44: 1000.0 (6'27"44)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=257842);
+-- SESENA BARBARA 400SL 6'11"21: 1008.59 (6'14"40)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1008.59
+  WHERE (`id`=257853);
+-- FRANCESCHINI STEFANO 400SL 5'49"31: 1029.17 (5'59"50)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1029.17
+  WHERE (`id`=257904);
+-- BERTOZZI ORLANDO 400SL 5'03"73: 1022.95 (5'10"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1022.95
+  WHERE (`id`=257909);
+-- LIGABUE MARCO 400SL 5'05"29: 982.31 (4'59"89)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=982.31
+  WHERE (`id`=257910);
+-- NOBILI CESARE 400SL 6'46"21: 1000.0 (6'46"21)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=257918);
+-- FERRI LUCA 400SL 7'16"37: 1000.0 (7'16"37)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=257929);
+-- DEL RIO SIMONE 50RA 38"10: 999.21 (38"07)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=999.21
+  WHERE (`id`=258219);
+-- SARAVO CRISTIAN 50RA 40"13: 941.94 (37"80)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=941.94
+  WHERE (`id`=258234);
+-- ALLORO STEFANO 50RA 35"63: 999.16 (35"60)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=999.16
+  WHERE (`id`=258265);
+-- VALCAVI LUCA 100FA 1'05"22: 984.82 (1'04"23)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=984.82
+  WHERE (`id`=257357);
+-- ORLANDINI IDO PIERALDO 100FA 1'23"94: 1007.86 (1'24"60)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1007.86
+  WHERE (`id`=257394);
+-- ZULIANI MATTEO 100FA 1'01"36: 956.65 (58"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=956.65
+  WHERE (`id`=257401);
+-- BENASSI SARA 50DO 43"29: 1000.0 (43"29)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=257953);
+-- VEZZANI GIORGIA 50DO 41"65: 1003.6 (41"80)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1003.6
+  WHERE (`id`=257979);
+-- SARAVO CRISTIAN 50DO 39"37: 937.26 (36"90)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=937.26
+  WHERE (`id`=258001);
+-- ZULIANI MATTEO 50DO 31"60: 952.53 (30"10)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=952.53
+  WHERE (`id`=258037);
+-- BENASSI SARA 100SL 1'22"17: 1000.0 (1'22"17)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=257535);
+-- VEZZANI GIORGIA 100SL 1'16"00: 1006.58 (1'16"50)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1006.58
+  WHERE (`id`=257564);
+-- VALCAVI LUCA 100SL 1'00"07: 1005.83 (1'00"42)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1005.83
+  WHERE (`id`=257571);
+-- GIANSOLDATI MATTEO 100SL 1'00"27: 1000.0 (1'00"27)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=257572);
+-- BONDI ANDREA 100SL 1'04"19: 983.02 (1'03"10)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=983.02
+  WHERE (`id`=257575);
+-- SADDI FRANCISCO JAVIER 100SL 1'05"75: 1002.28 (1'05"90)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1002.28
+  WHERE (`id`=257578);
+-- BIANCHI ELENA 200RA 3'23"37: 1019.52 (3'27"34)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1019.52
+  WHERE (`id`=257692);
+-- DEL RIO SIMONE 200RA 3'10"22: 982.86 (3'06"96)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=982.86
+  WHERE (`id`=257704);
+-- LIGABUE MARCO 200RA 2'57"78: 1044.61 (3'05"71)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1044.61
+  WHERE (`id`=257716);
+-- BUDASSI VALENTINA 200SL 2'32"91: 1057.48 (2'41"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1057.48
+  WHERE (`id`=257737);
+-- CANTONI SAMANTA 200SL 3'04"17: 1027.58 (3'09"25)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1027.58
+  WHERE (`id`=257741);
+-- LEONARDI ELISA 200SL 3'18"09: 958.5 (3'09"87)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=958.5
+  WHERE (`id`=257751);
+-- SESENA BARBARA 200SL 2'53"19: 1023.5 (2'57"26)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1023.5
+  WHERE (`id`=257753);
+-- CANOVI GABRIELE 200SL 2'57"37: 1014.26 (2'59"90)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1014.26
+  WHERE (`id`=257787);
+-- FRANCESCHINI STEFANO 200SL 2'44"02: 1040.79 (2'50"71)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1040.79
+  WHERE (`id`=257796);
+-- RIZZIELLO FERNANDO 200SL 4'06"55: 1000.0 (4'06"55)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=257832);
+-- PEZZI STEFANIA 50FA 33"74: 1037.34 (35"00)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1037.34
+  WHERE (`id`=258051);
+-- GIANSOLDATI MATTEO 50FA 30"53: 1000.0 (30"53)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=258074);
+-- BRUNI SIMONE 50FA 38"58: 927.94 (35"80)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=927.94
+  WHERE (`id`=258111);
+-- ALLORO STEFANO 50FA 31"31: 1017.25 (31"85)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1017.25
+  WHERE (`id`=258123);
+-- RICCO` ORIO 50FA 1'17"99: 1000.0 (1'17"99)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=258150);
+-- BIANCHI ELENA 100RA 1'36"00: 998.96 (1'35"90)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=998.96
+  WHERE (`id`=257482);
+-- FORMENTINI DAVIDE 100RA 1'28"30: 996.6 (1'28"00)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=996.6
+  WHERE (`id`=257496);
+-- FORMENTINI DAVIDE 50SL 30"54: 997.71 (30"47)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=997.71
+  WHERE (`id`=258373);
+-- CANOVI GABRIELE 50SL 33"73: 1029.05 (34"71)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1029.05
+  WHERE (`id`=258401);
+-- RIZZIELLO FERNANDO 50SL 46"61: 1000.0 (46"61)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=258459);
+-- RICCO` ORIO 50SL 1'09"09: 1000.0 (1'09"09)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=258461);
+-- GHEZZI EUGENIO 50SL 53"30: 1000.0 (53"30)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=258462);
+-- PEZZI STEFANIA 100MI 1'17"65: 1037.99 (1'20"60)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1037.99
+  WHERE (`id`=257409);
+-- LEONARDI ELISA 100MI 1'43"87: 957.35 (1'39"44)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=957.35
+  WHERE (`id`=257416);
+-- BONDI ANDREA 100MI 1'13"69: 953.45 (1'10"26)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=953.45
+  WHERE (`id`=257432);
+-- BRUNI SIMONE 100MI 1'29"03: 936.76 (1'23"40)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=936.76
+  WHERE (`id`=257451);
+-- GHEZZI EUGENIO 100MI 2'25"85: 1000.0 (2'25"85)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=257469);
