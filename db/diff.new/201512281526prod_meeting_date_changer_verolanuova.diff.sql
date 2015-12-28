a24fd1b64d964b71bacbe57b31939a949c027ecc
Marco Ligabue <leegaweb@gmail.com>
2015-12-28 17:22:44 +0100

    Data import Verolanuova 2015

---
+++ db/diff.new/201512281526prod_meeting_date_changer_verolanuova.diff.sql
@@ -0,0 +1,18 @@
+--
+-- Changing meeting 15215-verolanuova from 2015-10-24 to 2015-10-31
+-- 28 December 2015 16:26:54
+-- Begin script
+--
+
+UPDATE `meetings`
+  SET `header_date`='2015-10-31'
+  WHERE (`id`=15215);
+UPDATE `meeting_sessions`
+  SET `scheduled_date`='2015-10-31'
+  WHERE (`id`=1000);
+UPDATE `meeting_sessions`
+  SET `scheduled_date`='2015-11-01'
+  WHERE (`id`=1001);
+
+-- 15215-verolanuova date change done
+-- Script ended
---
+++ db/diff.new/201512281529prod_ris20151031verolanuova.12.diff.sql
@@ -0,0 +1,16 @@
+-- *** SQL Diff file for ris20151031verolanuova.txt ***
+-- Timestamp: 201512281529
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1765, NULL, 'Carisa Rari Nantes', 0, 653, 152, 1, '2015-12-28 15:40:09', '2015-12-28 15:40:09', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1766, NULL, 'G.A.M. TEAM', 0, 49, 152, 1, '2015-12-28 15:40:13', '2015-12-28 15:40:13', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1767, NULL, 'REZZATO N', 0, 60, 152, 1, '2015-12-28 15:40:15', '2015-12-28 15:40:15', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1768, NULL, 'SONCINO SC SRL', 0, 73, 152, 1, '2015-12-28 15:40:17', '2015-12-28 15:40:17', 1);
+
+
+-- Last completed phase code: 12
