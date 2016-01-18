b6c04b08ca54e46f6bc625750281738103d9a22b
Marco Ligabue <marco@leega-linux.homenet.telecomitalia.it>
2016-01-18 18:49:31 +0100

    A bit of ober cup

---
+++ db/diff.new/201601181848prod_goggle_cup_calc_10.diff.sql
@@ -0,0 +1,91 @@
+-- GoggleCup: Ober Cup 2016
+
+-- Meeting  : XIV TROFEO CITTA' DI RAVENNA
+-- BUDASSI VALENTINA 200SL 2'36"58: 982.25 (2'33"80)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=982.25
+  WHERE (`id`=269527);
+-- VALCAVI LUCA 200SL 2'15"80: 1021.94 (2'18"78)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1021.94
+  WHERE (`id`=269558);
+-- BERTOZZI ORLANDO 200SL 2'24"88: 994.82 (2'24"13)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=994.82
+  WHERE (`id`=269587);
+-- SESENA BARBARA 100DO 1'34"13: 987.57 (1'32"96)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=987.57
+  WHERE (`id`=269139);
+-- CANTONI SAMANTA 50SL 39"07: 1000.0 (39"07)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=270128);
+-- FRANCESCHINI STEFANO 50SL 34"31: 984.84 (33"79)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=984.84
+  WHERE (`id`=270260);
+-- DEL RIO SIMONE 50RA 38"85: 1004.89 (39"04)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1004.89
+  WHERE (`id`=270027);
+-- ALLORO STEFANO 50RA 36"19: 970.99 (35"14)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=970.99
+  WHERE (`id`=270067);
+-- CANTONI SAMANTA 400SL 6'43"09: 1010.62 (6'47"37)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1010.62
+  WHERE (`id`=269616);
+-- LEONARDI ELISA 400SL 6'53"25: 998.19 (6'52"50)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=998.19
+  WHERE (`id`=269635);
+-- TOFFANETTI LAURA 400SL 6'55"93: 1013.87 (7'01"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1013.87
+  WHERE (`id`=269636);
+-- SESENA BARBARA 400SL 6'20"46: 994.82 (6'18"49)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=994.82
+  WHERE (`id`=269643);
+-- DEL RIO SIMONE 400SL 6'01"22: 1000.0 (6'01"22)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=269671);
+-- FRANCESCHINI STEFANO 400SL 5'55"80: 1045.25 (6'11"90)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1045.25
+  WHERE (`id`=269695);
+-- VALCAVI LUCA 200MI 2'35"31: 972.12 (2'30"98)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=972.12
+  WHERE (`id`=269497);
+-- LIGABUE MARCO 200MI 2'49"25: 972.47 (2'44"59)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=972.47
+  WHERE (`id`=269513);
+-- BUDASSI VALENTINA 100SL 1'13"32: 1000.0 (1'13"32)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=269307);
+-- BERTOZZI ORLANDO 100SL 1'05"16: 1006.14 (1'05"56)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1006.14
+  WHERE (`id`=269448);
+-- LIGABUE MARCO 100RA 1'27"62: 964.39 (1'24"50)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=964.39
+  WHERE (`id`=269280);
+-- SARAVO CRISTIAN 50FA 33"93: 923.08 (31"32)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=923.08
+  WHERE (`id`=269932);
+-- ALLORO STEFANO 50FA 33"85: 939.44 (31"80)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=939.44
+  WHERE (`id`=269955);
+-- SARAVO CRISTIAN 50DO 40"31: 953.11 (38"42)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=953.11
+  WHERE (`id`=269823);
