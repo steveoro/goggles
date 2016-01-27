abe3e537a1059fbf87515f333bcd7c7c89920280
Marco Ligabue <leegaweb@gmail.com>
2016-01-27 16:29:32 +0100

    15266 import

---
+++ db/diff.new/201601271418prod_ris20160124bolzanodelledolomiti.12.diff.sql
@@ -0,0 +1,9 @@
+-- *** SQL Diff file for ris20160124bolzanodelledolomiti.txt ***
+-- Timestamp: 201601271418
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (1938, NULL, 'R.N. VALSUGANA', 0, 595, 152, 1, '2016-01-27 14:34:35', '2016-01-27 14:34:35', 1);
+
+UPDATE `meetings` SET `notes`='Bolzano Nuoto Master' WHERE (`id`=15266);
+
+
+-- Last completed phase code: 12
