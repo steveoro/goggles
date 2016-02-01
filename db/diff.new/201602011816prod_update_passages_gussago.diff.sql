32d45a6dfaa1b896d16beecd062f09ce60e163ef
marco <marco@linux-5i6x.homenet.telecomitalia.it>
2016-02-01 18:19:34 +0100

    Gussago passages

---
+++ db/diff.new/201602011816prod_update_passages_gussago.diff.sql
@@ -0,0 +1,112 @@
+--
+-- PassagesBatchUpdater: recorded from actions by [User: leega (Marco Ligabue) ID: 2]
+-- 01 February 2016 18:16:03
+-- Begin script
+--
+
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=39, `hundreds`=0, `user_id`=2, `minutes_from_start`=0, `seconds_from_start`=39, `hundreds_from_start`=0
+  WHERE (`id`=4815);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=48, `hundreds`=49, `user_id`=2, `minutes_from_start`=1, `seconds_from_start`=27, `hundreds_from_start`=49
+  WHERE (`id`=4816);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=48, `hundreds`=80, `user_id`=2, `minutes_from_start`=2, `seconds_from_start`=16, `hundreds_from_start`=29
+  WHERE (`id`=4817);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=40, `hundreds`=43, `user_id`=2, `minutes_from_start`=2, `seconds_from_start`=56, `hundreds_from_start`=72
+  WHERE (`id`=4818);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=33, `hundreds`=91, `user_id`=2, `minutes_from_start`=0, `seconds_from_start`=33, `hundreds_from_start`=91
+  WHERE (`id`=4819);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=44, `hundreds`=5, `user_id`=2, `minutes_from_start`=1, `seconds_from_start`=17, `hundreds_from_start`=96
+  WHERE (`id`=4820);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=44, `hundreds`=88, `user_id`=2, `minutes_from_start`=2, `seconds_from_start`=2, `hundreds_from_start`=84
+  WHERE (`id`=4821);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=37, `hundreds`=63, `user_id`=2, `minutes_from_start`=2, `seconds_from_start`=40, `hundreds_from_start`=47
+  WHERE (`id`=4822);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=30, `hundreds`=67, `user_id`=2, `minutes_from_start`=0, `seconds_from_start`=30, `hundreds_from_start`=67
+  WHERE (`id`=4823);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=39, `hundreds`=23, `user_id`=2, `minutes_from_start`=1, `seconds_from_start`=9, `hundreds_from_start`=90
+  WHERE (`id`=4824);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=43, `hundreds`=60, `user_id`=2, `minutes_from_start`=1, `seconds_from_start`=53, `hundreds_from_start`=50
+  WHERE (`id`=4825);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=33, `hundreds`=18, `user_id`=2, `minutes_from_start`=2, `seconds_from_start`=26, `hundreds_from_start`=68
+  WHERE (`id`=4826);
+DELETE FROM `passages` WHERE (`id`=4827);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=42, `hundreds`=14, `user_id`=2, `minutes_from_start`=0, `seconds_from_start`=42, `hundreds_from_start`=14
+  WHERE (`id`=4828);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=45, `hundreds`=67, `user_id`=2, `minutes_from_start`=1, `seconds_from_start`=27, `hundreds_from_start`=81
+  WHERE (`id`=4829);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=46, `hundreds`=89, `user_id`=2, `minutes_from_start`=2, `seconds_from_start`=14, `hundreds_from_start`=70
+  WHERE (`id`=4830);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=48, `hundreds`=5, `user_id`=2, `minutes_from_start`=3, `seconds_from_start`=2, `hundreds_from_start`=75
+  WHERE (`id`=4831);
+UPDATE `passages`
+  SET `minutes`=3, `seconds`=0, `hundreds`=34, `user_id`=2, `minutes_from_start`=3, `seconds_from_start`=0, `hundreds_from_start`=34
+  WHERE (`id`=4832);
+UPDATE `passages`
+  SET `minutes`=2, `seconds`=55, `hundreds`=74, `user_id`=2, `minutes_from_start`=2, `seconds_from_start`=55, `hundreds_from_start`=74
+  WHERE (`id`=4833);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=36, `hundreds`=57, `user_id`=2, `minutes_from_start`=0, `seconds_from_start`=36, `hundreds_from_start`=57
+  WHERE (`id`=4834);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=41, `hundreds`=1, `user_id`=2, `minutes_from_start`=1, `seconds_from_start`=17, `hundreds_from_start`=58
+  WHERE (`id`=4835);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=42, `hundreds`=63, `user_id`=2, `minutes_from_start`=2, `seconds_from_start`=0, `hundreds_from_start`=21
+  WHERE (`id`=4836);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=42, `hundreds`=43, `user_id`=2, `minutes_from_start`=2, `seconds_from_start`=42, `hundreds_from_start`=64
+  WHERE (`id`=4837);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=32, `hundreds`=25, `user_id`=2, `minutes_from_start`=0, `seconds_from_start`=32, `hundreds_from_start`=25
+  WHERE (`id`=4838);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=34, `hundreds`=60, `user_id`=2, `minutes_from_start`=1, `seconds_from_start`=6, `hundreds_from_start`=85
+  WHERE (`id`=4839);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=35, `hundreds`=31, `user_id`=2, `minutes_from_start`=1, `seconds_from_start`=42, `hundreds_from_start`=16
+  WHERE (`id`=4840);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=35, `hundreds`=63, `user_id`=2, `minutes_from_start`=2, `seconds_from_start`=17, `hundreds_from_start`=79
+  WHERE (`id`=4841);
+DELETE FROM `passages` WHERE (`id`=4842);
+DELETE FROM `passages` WHERE (`id`=4843);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=41, `hundreds`=23, `user_id`=2, `minutes_from_start`=0, `seconds_from_start`=41, `hundreds_from_start`=23
+  WHERE (`id`=4844);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=42, `hundreds`=75, `user_id`=2, `minutes_from_start`=1, `seconds_from_start`=23, `hundreds_from_start`=98
+  WHERE (`id`=4845);
+DELETE FROM `passages` WHERE (`id`=4846);
+DELETE FROM `passages` WHERE (`id`=4847);
+DELETE FROM `passages` WHERE (`id`=4848);
+DELETE FROM `passages` WHERE (`id`=4849);
+DELETE FROM `passages` WHERE (`id`=4850);
+DELETE FROM `passages` WHERE (`id`=4851);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=32, `hundreds`=23, `user_id`=2, `minutes_from_start`=0, `seconds_from_start`=32, `hundreds_from_start`=23
+  WHERE (`id`=4852);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=37, `hundreds`=66, `user_id`=2, `minutes_from_start`=1, `seconds_from_start`=9, `hundreds_from_start`=89
+  WHERE (`id`=4853);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=38, `hundreds`=82, `user_id`=2, `minutes_from_start`=1, `seconds_from_start`=48, `hundreds_from_start`=71
+  WHERE (`id`=4854);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=38, `hundreds`=77, `user_id`=2, `minutes_from_start`=2, `seconds_from_start`=27, `hundreds_from_start`=48
+  WHERE (`id`=4855);
+DELETE FROM `passages` WHERE (`id`=4856);
