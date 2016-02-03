1d65c72a78c67bbf041a0727882d1af674a94ad0
steveoro <steve.alloro@gmail.com>
2016-02-03 17:36:44 +0100

    prod DB diff applied (minor data fix)

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -25,7 +25,7 @@ module Version
   MINOR   = '856'
 
   # Current build version.
-  BUILD   = '20160202'
+  BUILD   = '20160203'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
