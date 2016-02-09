def29f03f26546414b4d8538d8364848b1cf59f4
Marco Ligabue <leegaweb@gmail.com>
2016-02-09 12:39:23 +0100

    Relays and MORSIANI fix

---
+++ db/diff.new/201602091218prod_meeting_team_scores_15102csiprova3.diff.sql
@@ -0,0 +1,33 @@
+--
+-- Team scores calculation for Meeting 3A PROVA REGIONALE CSI 2015/2016
+-- 09 February 2016 12:18:14
+-- Begin script
+--
+
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 235.0, 0.0, 32, 15102, NULL, NULL, 0, 2, 30.0, 235.0, 0.0, 30.0, 235.0, 0.0, 30.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 883.0, 57.0, 17, 15102, NULL, NULL, 0, 2, 0.0, 883.0, 57.0, 0.0, 883.0, 57.0, 0.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 439.0, 33.0, 9, 15102, NULL, NULL, 0, 2, 18.0, 439.0, 33.0, 18.0, 439.0, 33.0, 18.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 483.0, 57.0, 11, 15102, NULL, NULL, 0, 2, 16.0, 483.0, 57.0, 16.0, 483.0, 57.0, 16.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 633.0, 60.0, 1, 15102, NULL, NULL, 0, 2, 10.0, 633.0, 60.0, 10.0, 633.0, 60.0, 10.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 447.0, 21.0, 18, 15102, NULL, NULL, 0, 2, 15.0, 447.0, 21.0, 15.0, 447.0, 21.0, 15.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 88.0, 8.0, 826, 15102, NULL, NULL, 0, 2, 27.0, 88.0, 8.0, 27.0, 88.0, 8.0, 27.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 142.0, 15.0, 4, 15102, NULL, NULL, 0, 2, 31.0, 142.0, 15.0, 31.0, 142.0, 15.0, 31.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 98.0, 9.0, 290, 15102, NULL, NULL, 0, 2, 32.0, 98.0, 9.0, 32.0, 98.0, 9.0, 32.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 36.0, 0.0, 35, 15102, NULL, NULL, 0, 2, 36.0, 36.0, 0.0, 36.0, 36.0, 0.0, 36.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 80.0, 0.0, 10, 15102, NULL, NULL, 0, 2, 34.0, 80.0, 0.0, 34.0, 80.0, 0.0, 34.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (NULL, 26.0, 0.0, 827, 15102, NULL, NULL, 0, 2, 37.0, 26.0, 0.0, 37.0, 26.0, 0.0, 37.0, 151, NULL);
+
+-- Team scores calculation for Meeting 3A PROVA REGIONALE CSI 2015/2016 done
+-- Script ended
