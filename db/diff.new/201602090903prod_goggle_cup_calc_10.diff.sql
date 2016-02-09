583ae2ed2a5a2bbc7949608b095bdd0fcecc76c2
leegaweb <leegaweb@gmail.com>
2016-02-09 09:10:22 +0100

    15102 relay and 50special This is only a test. We're waiting for some Negrini's issues

---
+++ db/diff.new/201602090903prod_goggle_cup_calc_10.diff.sql
@@ -0,0 +1,199 @@
+-- GoggleCup: Ober Cup 2016
+
+-- Meeting  : 3A PROVA REGIONALE CSI 2015/2016
+-- BERTOZZI ORLANDO 100SL 1'02"80: 1014.33 (1'03"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1014.33
+  WHERE (`id`=273018);
+-- ARNESANO RICCARDO 100SL 1'50"30: 1000.0 (1'50"30)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=272991);
+-- RONZONI ALESSANDRO 100SL 1'05"80: 993.92 (1'05"40)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=993.92
+  WHERE (`id`=273030);
+-- GANGAROSSA DAVIDE 100SL 1'20"30: 997.51 (1'20"10)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=997.51
+  WHERE (`id`=273000);
+-- STROZZI PIERANTONIO 100SL 1'21"10: 982.74 (1'19"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=982.74
+  WHERE (`id`=273033);
+-- BIANCHI ELENA 100SL 1'30"90: 987.9 (1'29"80)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=987.9
+  WHERE (`id`=273019);
+-- FRANCESCHINI STEFANO 100SL 1'12"80: 1057.69 (1'17"00)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1057.69
+  WHERE (`id`=272999);
+-- NASI NICOLE 100SL 1'01"40: 1022.8 (1'02"80)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1022.8
+  WHERE (`id`=273003);
+-- VEZZANI GIORGIA 100SL 1'12"00: 1062.5 (1'16"50)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1062.5
+  WHERE (`id`=273013);
+-- BURANI PIETRO 100SL 1'05"90: 987.86 (1'05"10)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=987.86
+  WHERE (`id`=273021);
+-- PEZZI STEFANIA 100SL 1'08"10: 1057.71 (1'12"03)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1057.71
+  WHERE (`id`=273029);
+-- ARNESANO RICCARDO 50DO 57"70: 1000.0 (57"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=273016);
+-- ALLORO STEFANO 50DO 40"20: 1017.41 (40"90)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1017.41
+  WHERE (`id`=273015);
+-- FORMENTINI DAVIDE 50DO 41"10: 978.1 (40"20)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=978.1
+  WHERE (`id`=272998);
+-- STROZZI PIERANTONIO 50DO 46"50: 937.63 (43"60)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=937.63
+  WHERE (`id`=273010);
+-- ATTOLINI FEDERICO 50DO 41"00: 1009.76 (41"40)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1009.76
+  WHERE (`id`=273017);
+-- SARAVO CRISTIAN 50DO 37"80: 976.19 (36"90)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=976.19
+  WHERE (`id`=273008);
+-- LEONARDI ELISA 50DO 50"40: 1000.0 (50"40)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=273001);
+-- TOFFANETTI LAURA 50DO 53"20: 1000.0 (53"20)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=273034);
+-- SESENA BARBARA 50DO 43"10: 988.4 (42"60)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=988.4
+  WHERE (`id`=273009);
+-- ZULIANI MATTEO 50DO 31"00: 970.97 (30"10)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=970.97
+  WHERE (`id`=273014);
+-- FERRARI ALESSIA 50DO 50"60: 978.26 (49"50)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=978.26
+  WHERE (`id`=273022);
+-- BERTOZZI ORLANDO 200SL 2'17"50: 1032.73 (2'22"00)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1032.73
+  WHERE (`id`=272993);
+-- LIGABUE MARCO 200SL 2'21"70: 982.29 (2'19"19)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=982.29
+  WHERE (`id`=273026);
+-- ORLANDINI IDO PIERALDO 200SL 2'43"80: 1003.66 (2'44"40)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1003.66
+  WHERE (`id`=273004);
+-- FRANCESCHINI STEFANO 200SL 2'43"50: 1044.1 (2'50"71)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1044.1
+  WHERE (`id`=273024);
+-- BIANCHI ELENA 200SL 3'16"60: 995.42 (3'15"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=995.42
+  WHERE (`id`=272994);
+-- BUDASSI VALENTINA 200SL 2'32"30: 1061.72 (2'41"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1061.72
+  WHERE (`id`=272995);
+-- PESARE REBECCA 200SL 2'22"80: 971.29 (2'18"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=971.29
+  WHERE (`id`=273005);
+-- PEZZI STEFANIA 200SL 2'31"50: 1077.23 (2'43"20)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1077.23
+  WHERE (`id`=273006);
+-- TOFFANETTI LAURA 200SL 3'13"80: 1015.94 (3'16"89)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1015.94
+  WHERE (`id`=273011);
+-- LEONARDI ELISA 200SL 3'12"20: 987.88 (3'09"87)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=987.88
+  WHERE (`id`=273025);
+-- VALCAVI LUCA 200SL 2'09"70: 1006.4 (2'10"53)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1006.4
+  WHERE (`id`=273012);
+-- RONZONI ALESSANDRO 50FA 35"10: 1058.4 (37"15)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1058.4
+  WHERE (`id`=273007);
+-- ZULIANI MATTEO 50FA 27"70: 989.17 (27"40)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=989.17
+  WHERE (`id`=273037);
+-- ATTOLINI FEDERICO 50FA 39"00: 984.62 (38"40)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=984.62
+  WHERE (`id`=272992);
+-- SARAVO CRISTIAN 50FA 33"10: 969.49 (32"09)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=969.49
+  WHERE (`id`=273031);
+-- SESENA BARBARA 50FA 43"40: 1080.65 (46"90)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1080.65
+  WHERE (`id`=273032);
+-- BURANI PIETRO 50FA 33"40: 1000.0 (33"40)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=272996);
+-- FORMENTINI DAVIDE 50FA 39"60: 992.42 (39"30)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=992.42
+  WHERE (`id`=273023);
+-- VALCAVI LUCA 50FA 29"70: 979.12 (29"08)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=979.12
+  WHERE (`id`=273035);
+-- ALLORO STEFANO 50FA 31"30: 1017.57 (31"85)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1017.57
+  WHERE (`id`=272990);
+-- LIGABUE MARCO 50FA 32"00: 996.88 (31"90)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=996.88
+  WHERE (`id`=273002);
+-- FERRARI ALESSIA 50FA 44"20: 996.61 (44"05)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=996.61
+  WHERE (`id`=272997);
+-- BUDASSI VALENTINA 50FA 38"20: 1057.59 (40"40)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1057.59
+  WHERE (`id`=273020);
+-- ORLANDINI IDO PIERALDO 50FA 37"70: 973.47 (36"70)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=973.47
+  WHERE (`id`=273027);
+-- PESARE REBECCA 50FA 32"50: 1003.08 (32"60)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1003.08
+  WHERE (`id`=273028);
+-- NASI NICOLE 50RA 34"90: 985.67 (34"40)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=985.67
+  WHERE (`id`=273232);
+-- ZULIANI MATTEO 50RA 39"80: 1000.0 (39"80)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=273241);
