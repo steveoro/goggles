0e16dd1ba7a631bc234babe771a4a34533d6657a
Leega <marco@leegahome.homenet.telecomitalia.it>
2016-03-07 23:28:25 +0100

    15104 csiprova4 relay results

---
+++ db/diff.new/201603072322prod_meeting_team_scores_15104csiprova4.diff.sql
@@ -0,0 +1,33 @@
+--
+-- Team scores calculation for Meeting 4A PROVA REGIONALE CSI 2015/2016
+-- 07 March 2016 23:22:48
+-- Begin script
+--
+
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10368, 363.0, 42.0, 9, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 20.0, 363.0, 42.0, 20.0, 363.0, 42.0, 20.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10369, 232.0, 39.0, 32, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 28.0, 232.0, 39.0, 28.0, 232.0, 39.0, 28.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10370, 610.0, 65.0, 17, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 4.0, 610.0, 65.0, 4.0, 610.0, 65.0, 4.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10371, 752.0, 93.0, 1, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 0.0, 752.0, 93.0, 0.0, 752.0, 93.0, 0.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10372, 312.0, 48.0, 18, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 14.0, 312.0, 48.0, 14.0, 312.0, 48.0, 14.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10373, 400.0, 66.0, 11, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 18.0, 400.0, 66.0, 18.0, 400.0, 66.0, 18.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10374, 591.0, 93.0, 5, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 8.0, 591.0, 93.0, 8.0, 591.0, 93.0, 8.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10375, 388.0, 60.0, 10, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 16.0, 388.0, 60.0, 16.0, 388.0, 60.0, 16.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10376, 49.0, 9.0, 35, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 36.0, 49.0, 9.0, 36.0, 49.0, 9.0, 36.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10377, 172.0, 30.0, 21, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 31.0, 172.0, 30.0, 31.0, 172.0, 30.0, 31.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10378, 91.0, 2.0, 4, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 35.0, 91.0, 2.0, 35.0, 91.0, 2.0, 35.0, 151, NULL);
+INSERT INTO `meeting_team_scores` (`id`, `sum_individual_points`, `sum_relay_points`, `team_id`, `meeting_id`, `created_at`, `updated_at`, `rank`, `user_id`, `sum_team_points`, `meeting_individual_points`, `meeting_relay_points`, `meeting_team_points`, `season_individual_points`, `season_relay_points`, `season_team_points`, `season_id`, `team_affiliation_id`)
+  VALUES (10379, 52.0, 2.0, 826, 15104, '2016-03-07 22:22:49', '2016-03-07 22:22:49', 0, 2, 33.0, 52.0, 2.0, 33.0, 52.0, 2.0, 33.0, 151, NULL);
+
+-- Team scores calculation for Meeting 4A PROVA REGIONALE CSI 2015/2016 done
+-- Script ended
