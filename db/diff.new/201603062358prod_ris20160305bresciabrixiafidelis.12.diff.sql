a90b9568e1ab72a2ab5290042ef1440c2a27835a
Leega <marco@leegahome.homenet.telecomitalia.it>
2016-03-07 01:20:52 +0100

    15217 bresciabrixiafidelis import

---
+++ db/diff.new/201603062358prod_ris20160305bresciabrixiafidelis.12.diff.sql
@@ -0,0 +1,18 @@
+-- *** SQL Diff file for ris20160305bresciabrixiafidelis.txt ***
+-- Timestamp: 201603062358
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (2031, NULL, 'TESEO TESEI PORTOFERRAIO', 0, 403, 152, 1, '2016-03-07 00:02:58', '2016-03-07 00:02:58', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (2032, NULL, 'A.S.DILETT. JESOLONUOTO', 0, 149, 152, 1, '2016-03-07 00:02:59', '2016-03-07 00:02:59', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (2033, NULL, 'ASD SERENISSIMA NUOTO CLU', 0, 346, 152, 1, '2016-03-07 00:03:00', '2016-03-07 00:03:00', 1);
+
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (2034, NULL, 'RN U.O.E.I. CANDIDO CABBIA ASD', 0, 170, 152, 1, '2016-03-07 00:03:02', '2016-03-07 00:03:02', 1);
+
+UPDATE `meetings` SET `notes`='ASD Sport & Fitness' WHERE (`id`=15217);
+
+
+-- Last completed phase code: 12
---
+++ db/diff.new/201603070116prod_goggle_cup_calc_10.diff.sql
@@ -0,0 +1,55 @@
+-- GoggleCup: Ober Cup 2016
+
+-- Meeting  : XXXI BRIXIA FIDELIS
+-- BUDASSI VALENTINA 800SL 10'55"07: 1022.35 (11'09"71)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1022.35
+  WHERE (`id`=286883);
+-- CANTONI SAMANTA 800SL 13'30"08: 1000.0 (13'30"08)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=286885);
+-- LEONARDI ELISA 800SL 14'18"23: 1000.0 (14'18"23)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=286899);
+-- GIANSOLDATI MATTEO 800SL 10'16"72: 1000.0 (10'16"72)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=286932);
+-- DEL RIO SIMONE 50RA 37"59: 1038.57 (39"04)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1038.57
+  WHERE (`id`=286459);
+-- LIGABUE MARCO 50RA 37"99: 1000.0 (37"99)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=286516);
+-- GHIRARDELLI CRISTIAN 50SL 29"01: 1000.0 (29"01)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=286672);
+-- LIGABUE MARCO 50SL 30"33: 989.78 (30"02)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=989.78
+  WHERE (`id`=286799);
+-- GIANSOLDATI MATTEO 50FA 30"48: 1000.0 (30"48)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=286299);
+-- GHIRARDELLI CRISTIAN 50FA 31"51: 1000.0 (31"51)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1000.0
+  WHERE (`id`=286301);
+-- DEL RIO SIMONE 50FA 37"03: 946.53 (35"05)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=946.53
+  WHERE (`id`=286329);
+-- VALCAVI LUCA 200SL 2'34"39: 898.89 (2'18"78)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=898.89
+  WHERE (`id`=286048);
+-- BUDASSI VALENTINA 400SL 5'19"00: 1017.3 (5'24"52)
+UPDATE `meeting_individual_results`
+  SET `goggle_cup_points`=1017.3
+  WHERE (`id`=286092);
