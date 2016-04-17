73d0fb90b216ee99dc75fa760eb029988de7698f
Leega <marco@leegahome.homenet.telecomitalia.it>
2016-04-18 00:56:15 +0200

    15105 csiprova5 import (without relays)

---
+++ db/diff.new/201604172216prod_ris20160417csiprova5.11.ok.sql
@@ -0,0 +1,15 @@
+--
+-- *** Suggested SQL actions: ***
+--
+
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
+
+-- Processing:...'SORAGNI NICOLO'' (1998, gender: 1)
+-- aliased with: 'SORAGNI NICOL�' (ID:24861)
+INSERT INTO `data_import_swimmer_aliases` (`id`, `created_at`, `updated_at`, `complete_name`, `swimmer_id`)
+  VALUES (378, '2016-04-17 22:17:29', '2016-04-17 22:17:29', 'SORAGNI NICOLO\'', 24861);
+
+
+COMMIT;
