fa604798d5d24b3ce10ffbec5190b596ad38637d
steveoro <steve.alloro@gmail.com>
2016-04-18 01:34:28 +0200

    db diffs updated on DB; dev & prod DB dumps updated

---
+++ db/diff.applied/201604161148dev_update_passages_molinella.diff.sql
@@ -0,0 +1,72 @@
+--
+-- PassagesBatchUpdater: recorded from actions by [User: Paul_Multisport (Paolo Orestilli) ID: 118]
+-- 16 April 2016 11:48:21
+-- Begin script
+--
+
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=25, `hundreds`=50, `user_id`=118, `minutes_from_start`=1, `seconds_from_start`=25, `hundreds_from_start`=50
+  WHERE (`id`=5215);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=36, `hundreds`=50, `user_id`=118, `minutes_from_start`=3, `seconds_from_start`=2, `hundreds_from_start`=0
+  WHERE (`id`=5216);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=35, `hundreds`=50, `user_id`=118, `minutes_from_start`=4, `seconds_from_start`=37, `hundreds_from_start`=50
+  WHERE (`id`=5217);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=36, `hundreds`=50, `user_id`=118, `minutes_from_start`=6, `seconds_from_start`=14, `hundreds_from_start`=0
+  WHERE (`id`=5218);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=33, `hundreds`=50, `user_id`=118, `minutes_from_start`=7, `seconds_from_start`=47, `hundreds_from_start`=50
+  WHERE (`id`=5219);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=33, `hundreds`=50, `user_id`=118, `minutes_from_start`=9, `seconds_from_start`=21, `hundreds_from_start`=0
+  WHERE (`id`=5220);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=31, `hundreds`=50, `user_id`=118, `minutes_from_start`=10, `seconds_from_start`=52, `hundreds_from_start`=50
+  WHERE (`id`=5221);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=36, `hundreds`=11, `user_id`=118, `minutes_from_start`=12, `seconds_from_start`=28, `hundreds_from_start`=61
+  WHERE (`id`=5222);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=17, `hundreds`=59, `user_id`=118, `minutes_from_start`=1, `seconds_from_start`=17, `hundreds_from_start`=59
+  WHERE (`id`=5223);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=21, `hundreds`=0, `user_id`=118, `minutes_from_start`=2, `seconds_from_start`=38, `hundreds_from_start`=59
+  WHERE (`id`=5224);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=20, `hundreds`=75, `user_id`=118, `minutes_from_start`=3, `seconds_from_start`=59, `hundreds_from_start`=34
+  WHERE (`id`=5225);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=22, `hundreds`=23, `user_id`=118, `minutes_from_start`=5, `seconds_from_start`=21, `hundreds_from_start`=57
+  WHERE (`id`=5226);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=22, `hundreds`=14, `user_id`=118, `minutes_from_start`=6, `seconds_from_start`=43, `hundreds_from_start`=71
+  WHERE (`id`=5227);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=22, `hundreds`=36, `user_id`=118, `minutes_from_start`=8, `seconds_from_start`=6, `hundreds_from_start`=7
+  WHERE (`id`=5228);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=23, `hundreds`=20, `user_id`=118, `minutes_from_start`=9, `seconds_from_start`=29, `hundreds_from_start`=27
+  WHERE (`id`=5229);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=20, `hundreds`=96, `user_id`=118, `minutes_from_start`=10, `seconds_from_start`=50, `hundreds_from_start`=23
+  WHERE (`id`=5230);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=14, `hundreds`=92, `user_id`=118, `minutes_from_start`=0, `seconds_from_start`=14, `hundreds_from_start`=92
+  WHERE (`id`=5231);
+UPDATE `passages`
+  SET `minutes`=0, `seconds`=16, `hundreds`=83, `user_id`=118, `minutes_from_start`=0, `seconds_from_start`=31, `hundreds_from_start`=75
+  WHERE (`id`=5232);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=15, `hundreds`=57, `user_id`=118, `minutes_from_start`=1, `seconds_from_start`=15, `hundreds_from_start`=57
+  WHERE (`id`=5233);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=20, `hundreds`=52, `user_id`=118, `minutes_from_start`=2, `seconds_from_start`=36, `hundreds_from_start`=9
+  WHERE (`id`=5234);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=20, `hundreds`=54, `user_id`=118, `minutes_from_start`=3, `seconds_from_start`=56, `hundreds_from_start`=63
+  WHERE (`id`=5235);
+UPDATE `passages`
+  SET `minutes`=1, `seconds`=17, `hundreds`=21, `user_id`=118, `minutes_from_start`=5, `seconds_from_start`=13, `hundreds_from_start`=84
+  WHERE (`id`=5236);
