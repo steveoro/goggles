f5a404ba47565ce3cfe56703b75f755e3442fd9d
Leega <marco@leegahome.homenet.telecomitalia.it>
2016-04-18 20:08:05 +0200

    Fix csiprova6 meeting date

---
+++ db/diff.new/201604181959prod_meeting_date_changer_csiprova6.diff.sql
@@ -0,0 +1,18 @@
+--
+-- Changing meeting 15106-csiprova6 from 2016-06-06 to 2016-06-05
+-- 18 April 2016 19:59:26
+-- Begin script
+--
+
+UPDATE `meetings`
+  SET `header_date`='2016-06-05'
+  WHERE (`id`=15106);
+UPDATE `meeting_sessions`
+  SET `scheduled_date`='2016-06-05'
+  WHERE (`id`=954);
+UPDATE `meeting_sessions`
+  SET `scheduled_date`='2016-06-05'
+  WHERE (`id`=955);
+
+-- 15106-csiprova6 date change done
+-- Script ended
