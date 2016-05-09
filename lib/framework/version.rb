0047acf425be3e11c4ed32c45fc17411d187f755
steveoro <steve.alloro@gmail.com>
2016-05-09 23:11:24 +0200

    DB diffs applied on prod; prod DB dump updated, dev DB dump *cloned* from prod (after diff:apply)

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '4.00'
 
   # Minor version.
-  MINOR   = '869'
+  MINOR   = '871'
 
   # Current build version.
-  BUILD   = '20160503'
+  BUILD   = '20160509'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
