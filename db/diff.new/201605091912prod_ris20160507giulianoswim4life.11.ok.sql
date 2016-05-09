a09312d26bc7c433b0cef6b6e6a6db9c860a51f9
Leega <marco@leegahome.homenet.telecomitalia.it>
2016-05-09 21:35:11 +0200

    Last WE imoprt

---
+++ db/diff.new/201605091912prod_ris20160507giulianoswim4life.11.ok.sql
@@ -0,0 +1,40 @@
+--
+-- *** Suggested SQL actions: ***
+--
+
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
+
+-- Processing:...'A.S.Dilett. Olbia Nuoto':
+-- aliased with: 'OLBIA NUOTO ASD' (ID:546)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (694, '2016-05-09 19:15:05', '2016-05-09 19:15:05', 'A.S.Dilett. Olbia Nuoto', 546);
+
+
+-- Processing:...'Athena Bracciano':
+-- aliased with: 'ATHENA SP.CLUB -BRA' (ID:693)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (695, '2016-05-09 19:15:05', '2016-05-09 19:15:05', 'Athena Bracciano', 693);
+
+
+-- Processing:...'Circolo Villani Ssd':
+-- aliased with: 'Circ.Villani Ssd Arl' (ID:931)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (696, '2016-05-09 19:15:05', '2016-05-09 19:15:05', 'Circolo Villani Ssd', 931);
+
+
+COMMIT;
+--
+-- *** Suggested SQL actions: ***
+--
+
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
+
+-- Processing:...'CORVIETTO FRANCESCO' (1986, gender: 1)
+
+-- Processing:...'LA SALA GENNARO' (1961, gender: 1)
+
+COMMIT;
---
+++ db/diff.new/201605091912prod_ris20160507giulianoswim4life.12.diff.sql
@@ -0,0 +1,9 @@
+-- *** SQL Diff file for ris20160507giulianoswim4life.txt ***
+-- Timestamp: 201605091912
+UPDATE `meetings` SET `notes`='ASD Swim4life' WHERE (`id`=15326);
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (6846, 6, 37, 85, 152, 3, 1, 34, 932, '2016-05-09 19:17:51', '2016-05-09 19:17:51');
+
+
+-- Last completed phase code: 12
