2e128bd69b82bf30dde4bbabfdefe4d303d3d75d
Leega <marco@leegahome.homenet.telecomitalia.it>
2016-06-03 17:40:55 +0200

    London!

---
+++ db/diff.new/201606031708prod_personal_best_finder_1025.diff.sql
@@ -0,0 +1,138 @@
+--
+-- Scanning swimmer VALCAVI LUCA [1025] for personal bests
+-- 03 June 2016 17:08:00
+-- Begin script
+--
+
+update meeting_individual_results set is_personal_best = false where swimmer_id = 1025 and is_personal_best = true;
+-- VALCAVI LUCA 50 STILE LIBERO - 25 metri:  0'27"20
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=255122);
+-- VALCAVI LUCA 50 STILE LIBERO - 50 metri:  0'28"30
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=2181);
+-- VALCAVI LUCA 100 STILE LIBERO - 25 metri:  0'59"49
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=254457);
+-- VALCAVI LUCA 100 STILE LIBERO - 50 metri:  1'03"87
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=101998);
+-- VALCAVI LUCA 200 STILE LIBERO - 25 metri:  2'09"70
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=273012);
+-- VALCAVI LUCA 200 STILE LIBERO - 50 metri:  2'14"92
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=436724);
+-- VALCAVI LUCA 400 STILE LIBERO - 25 metri:  4'37"30
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=255166);
+-- VALCAVI LUCA 400 STILE LIBERO - 50 metri:  4'54"30
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=323585);
+-- VALCAVI LUCA 800 STILE LIBERO - 25 metri:  9'55"10
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=17253);
+-- VALCAVI LUCA 800 STILE LIBERO - 50 metri: 10'40"63
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=103530);
+-- VALCAVI LUCA 1500 STILE LIBERO - 50 metri: 21'28"70
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=103145);
+-- VALCAVI LUCA 50 FARFALLA - 25 metri:  0'29"08
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=110960);
+-- VALCAVI LUCA 50 FARFALLA - 50 metri:  0'29"33
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=103806);
+-- VALCAVI LUCA 100 FARFALLA - 25 metri:  1'04"23
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=110975);
+-- VALCAVI LUCA 100 FARFALLA - 50 metri:  1'05"49
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=255547);
+-- VALCAVI LUCA 200 FARFALLA - 25 metri:  2'23"43
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=226114);
+-- VALCAVI LUCA 200 FARFALLA - 50 metri:  2'27"59
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=237686);
+-- VALCAVI LUCA 50 DORSO - 25 metri:  0'32"80
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=13632);
+-- VALCAVI LUCA 50 DORSO - 50 metri:  0'34"00
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=12982);
+-- VALCAVI LUCA 100 DORSO - 25 metri:  1'11"60
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=17551);
+-- VALCAVI LUCA 100 DORSO - 50 metri:  1'13"42
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=101675);
+-- VALCAVI LUCA 200 DORSO - 25 metri:  2'36"00
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=103286);
+-- VALCAVI LUCA 200 DORSO - 50 metri:  2'39"50
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=16789);
+-- VALCAVI LUCA 50 RANA - 25 metri:  0'35"33
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=103343);
+-- VALCAVI LUCA 100 RANA - 25 metri:  1'18"90
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=423);
+-- VALCAVI LUCA 100 RANA - 50 metri:  1'25"01
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=103538);
+-- VALCAVI LUCA 200 RANA - 25 metri:  2'55"03
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=103528);
+-- VALCAVI LUCA 100 MISTI - 25 metri:  1'08"20
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=17924);
+-- VALCAVI LUCA 200 MISTI - 25 metri:  2'26"68
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=271969);
+-- VALCAVI LUCA 200 MISTI - 50 metri:  2'30"30
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=436717);
+-- VALCAVI LUCA 400 MISTI - 25 metri:  5'12"01
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=226383);
+-- VALCAVI LUCA 400 MISTI - 50 metri:  5'24"49
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=110964);
+
+-- Swimmer VALCAVI LUCA: 30 personal bests found
+-- Script ended
---
+++ db/diff.new/201606031708prod_personal_best_finder_1227.diff.sql
@@ -0,0 +1,70 @@
+--
+-- Scanning swimmer BERTOZZI ORLANDO [1227] for personal bests
+-- 03 June 2016 17:08:14
+-- Begin script
+--
+
+update meeting_individual_results set is_personal_best = false where swimmer_id = 1227 and is_personal_best = true;
+-- BERTOZZI ORLANDO 50 STILE LIBERO - 25 metri:  0'28"10
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=14499);
+-- BERTOZZI ORLANDO 50 STILE LIBERO - 50 metri:  0'28"90
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=16797);
+-- BERTOZZI ORLANDO 100 STILE LIBERO - 25 metri:  1'02"10
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=13792);
+-- BERTOZZI ORLANDO 100 STILE LIBERO - 50 metri:  1'03"90
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=14681);
+-- BERTOZZI ORLANDO 200 STILE LIBERO - 25 metri:  2'17"50
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=272993);
+-- BERTOZZI ORLANDO 200 STILE LIBERO - 50 metri:  2'21"86
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=436723);
+-- BERTOZZI ORLANDO 400 STILE LIBERO - 25 metri:  5'03"10
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=940);
+-- BERTOZZI ORLANDO 400 STILE LIBERO - 50 metri:  5'16"50
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=323701);
+-- BERTOZZI ORLANDO 800 STILE LIBERO - 25 metri: 10'41"90
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=19431);
+-- BERTOZZI ORLANDO 800 STILE LIBERO - 50 metri: 11'14"40
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=1753);
+-- BERTOZZI ORLANDO 50 FARFALLA - 25 metri:  0'33"40
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=14519);
+-- BERTOZZI ORLANDO 50 RANA - 25 metri:  0'37"80
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=15683);
+-- BERTOZZI ORLANDO 100 MISTI - 25 metri:  1'15"30
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=232817);
+-- BERTOZZI ORLANDO 200 MISTI - 25 metri:  2'55"10
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=21279);
+-- BERTOZZI ORLANDO 200 MISTI - 50 metri:  2'53"70
+UPDATE `meeting_individual_results`
+  SET `is_personal_best`=1
+  WHERE (`id`=16820);
+
+-- Swimmer BERTOZZI ORLANDO: 14 personal bests found
+-- Script ended
