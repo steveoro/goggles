a12a9a6e529b2166df4ca7b32d2d439606004d85
steveoro <steve.alloro@gmail.com>
2016-06-28 17:06:17 +0200

    removed legacy relation with non-existent model

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '4.00'
 
   # Minor version.
-  MINOR   = '881'
+  MINOR   = '883'
 
   # Current build version.
-  BUILD   = '20160620'
+  BUILD   = '20160628'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
