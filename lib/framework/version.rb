2789900844c286f1f71ddfb06799012888b5732b
steveoro <steve.alloro@gmail.com>
2016-07-18 18:25:19 +0200

    improved Capfile & Capistrano config; applied DB diffs on prod; moved seed dir & generators to Admin app

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -25,7 +25,7 @@ module Version
   MINOR   = '002'
 
   # Current build version.
-  BUILD   = '20160715'
+  BUILD   = '20160718'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
