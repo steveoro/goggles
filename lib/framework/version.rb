fffc288e8de255525b3021e967500b82013bd5d0
steveoro <steve.alloro@gmail.com>
2016-12-07 18:20:54 +0100

    improved landing page w/ left & right shortcut buttons even on xs-display sizes

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '6'
 
   # Minor version.
-  MINOR   = '025'
+  MINOR   = '026'
 
   # Current build version.
-  BUILD   = '20161206'
+  BUILD   = '20161207'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
