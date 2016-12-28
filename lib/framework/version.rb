2dd58e4fb1d82adcbc5272d2b9d4e28329bedf50
steveoro <steve.alloro@gmail.com>
2016-12-28 20:58:11 +0100

    minor layout improvements

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '6'
 
   # Minor version.
-  MINOR   = '036'
+  MINOR   = '037'
 
   # Current build version.
-  BUILD   = '20161227'
+  BUILD   = '20161228'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
