cd98c9d526e2bcbb8f710ccff781ce9adc996496
steveoro <steve.alloro@gmail.com>
2017-01-24 09:57:59 +0100

    quick fix for meeting_reservations#show_relay_config

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '6'
 
   # Minor version.
-  MINOR   = '060'
+  MINOR   = '061'
 
   # Current build version.
-  BUILD   = '20170123'
+  BUILD   = '20170124'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
