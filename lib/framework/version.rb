1b6adf7c308ee4f3c48bcba0470f7a07c8c889ec
steveoro <steve.alloro@gmail.com>
2017-02-22 13:44:48 +0100

    synch w/ updated core; version update

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '6'
 
   # Minor version.
-  MINOR   = '082'
+  MINOR   = '083'
 
   # Current build version.
-  BUILD   = '20170217'
+  BUILD   = '20170222'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
