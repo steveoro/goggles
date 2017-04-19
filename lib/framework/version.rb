598d8b35c77cc240bf8294dbad67177fc585d3c3
steveoro <steve.alloro@gmail.com>
2017-04-19 11:29:34 +0200

    - improved contact-us buttons w/ dedicated social-media partial - fix view bug in home page

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '6'
 
   # Minor version.
-  MINOR   = '102'
+  MINOR   = '103'
 
   # Current build version.
-  BUILD   = '20170414'
+  BUILD   = '20170419'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
