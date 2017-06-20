46df406d6fd7099a969576f0b487597404e41c03
steveoro <steve.alloro@gmail.com>
2017-06-20 13:48:38 +0200

    moved "my meetings" button to the left shortcuts partial in home page

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -25,7 +25,7 @@ module Version
   MINOR   = '110'
 
   # Current build version.
-  BUILD   = '20170521'
+  BUILD   = '20170620'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
