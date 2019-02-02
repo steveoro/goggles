7d7b356c3557a1161dbf2cf4a0e7427773ceed01
Marco Ligabue <leega@leegax.lan>
2019-02-02 11:09:36 +0100

    Presence checker devolping...

--- app/strategies/swimmer_presence_checker.rb
+++ app/strategies/swimmer_presence_checker.rb
@@ -56,11 +56,62 @@ class SwimmerPresenceChecker
   # Get the seasons for the given header year where swimmer has badge
   #
   def get_swimmer_current_seasons()
-    Season.where( header_year: get_current_header_year() ).where( ['exists (select 1 from badges where swimmer_id=?)', @swimmer.id] )  
+    Season.where( header_year: get_current_header_year() ).where( ['exists (select 1 from badges where season_id = seasons.id and swimmer_id=?)', @swimmer.id] )  
   end
   #-- -------------------------------------------------------------------------
   #++
     
+  # Check if the swimmer has attended the given meeting
+  # The swimmer has attended when there is an entry or an individual result or a confirmed reservation
+  #
+  def has_swimmer_attended_meeting( meeting )
+    is_present = false
+    # Check for meeting individual results
+    if MeetingIndividualResult.joins( :meeting_session ).where( ['meeting_individual_results.swimmer_id = ? and meeting_sessions.meeting_id = ?', @swimmer.id, meeting.id] ).exists?
+      is_present = true
+    # Check for meeting entries
+    elsif MeetingEntry.joins( :meeting_session ).where( ['meeting_entries.swimmer_id = ? and meeting_sessions.meeting_id = ?', @swimmer.id, meeting.id] ).exists?
+      is_present = true
+    # Check for confirmed reservation
+    elsif MeetingReservation.is_coming.where( ['has_confirmed and swimmer_id = ? and meeting_id = ?', @swimmer.id, meeting.id] ).exists?
+      is_present = true
+    end  
+    is_present
+  end
+  #-- -------------------------------------------------------------------------
+  #++
   
+  # Check if the swimmer swam in a relay for a given meeting
+  #
+  def has_swimmer_swam_relay( meeting )
+    MeetingRelayResult.joins( :meeting_session, :meeting_relay_swimmers ).where( ['meeting_relay_swimmers.swimmer_id = ? and meeting_sessions.meeting_id = ?', @swimmer.id, meeting.id] ).exists?
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+    
+  # Count swimmer events for the given meeting
+  # The swimmer events are the highest in entries, individual results and confirmed reservation events
+  #
+  def count_swimmer_events( meeting )
+    events = 0
+
+    # Count meeting individual results
+    mir_events = MeetingIndividualResult.joins( :meeting_session ).where( ['meeting_individual_results.swimmer_id = ? and meeting_sessions.meeting_id = ?', @swimmer.id, meeting.id] ).count
+    # Count meeting entries
+    entry_events = MeetingEntry.joins( :meeting_session ).where( ['meeting_entries.swimmer_id = ? and meeting_sessions.meeting_id = ?', @swimmer.id, meeting.id] ).count
+    # Count confirmed reservation
+    res_events = MeetingEventReservation.where( ['meeting_event_reservations.swimmer_id = ? and meeting_event_reservations.meeting_id = ? and meeting_event_reservations.is_doing_this', @swimmer.id, meeting.id] ).exists?
+
+    events
+  end
+  #-- -------------------------------------------------------------------------
+  #++
   
+  # Count swimmer's relays swam for a given meeting
+  #
+  def count_swimmer_relays( meeting )
+    MeetingRelayResult.joins( :meeting_session, :meeting_relay_swimmers ).where( ['meeting_relay_swimmers.swimmer_id = ? and meeting_sessions.meeting_id = ?', @swimmer.id, meeting.id] ).count
+  end
+  #-- -------------------------------------------------------------------------
+  #++
 end
--- spec/strategies/swimmer_presence_checker_spec.rb
+++ spec/strategies/swimmer_presence_checker_spec.rb
@@ -3,17 +3,33 @@ require 'rails_helper'
 
 describe SwimmerPresenceChecker, type: :strategy do
 
-  let(:fix_swimmer)       { Swimmer.find(23) }
-  let(:fix_date)          { Date.today() - 100 + (rand * 200).to_i }
+  let(:fix_swimmer)       { Swimmer.find( [23, 51, 142, 149].sample ) }  # Leega, Attolini, Steve, Ganga
+  let(:new_swimmer )      { create( :swimmer ) }
+  let(:leega)             { Swimmer.find( 23 ) }
+  let(:ganga)             { Swimmer.find( 149 ) }
 
-  let(:fix_pace_type )    { ['A2','B1','B2'].sample }
+  let(:fix_date)          { Date.today() - 100 + (rand * 200).to_i }
+  let(:date_1819)         { Date.new(2018, 12, 31) } 
+
+  let(:fix_meeting )    { Meeting.find( [18101,18205,18102,17224,18224,17205].sample ) }
+  let(:new_meeting )    { create( :meeting ) }
 
   subject { SwimmerPresenceChecker.new(fix_swimmer, fix_date) }
 
   describe "[a well formed instance]" do
-    it_behaves_like( "(the existance of a method returning strings)", [
+    it_behaves_like( "(the existance of a method returning non-empty strings)", [
       :get_current_header_year
     ])
+    
+    it_behaves_like( "(the existance of a method with parameters, returning boolean values)", [
+      :has_swimmer_attended_meeting,
+      :has_swimmer_swam_relay
+    ], Meeting.find(18224))
+
+    it_behaves_like( "(the existance of a method with parameters, returning numeric values)", [
+      :count_swimmer_relays,
+      :count_swimmer_events
+    ], Meeting.find(18224))
   end
   #-- -------------------------------------------------------------------------
   #++
@@ -38,8 +54,14 @@ describe SwimmerPresenceChecker, type: :strategy do
       expect( badges ).to all(be_an_instance_of( Badge ))
     end
 
-    it "returns the same number as current seasons" do
-      expect( subject.get_swimmer_current_badges().count ).to eq( subject.get_swimmer_current_seasons().count )
+    it "returns 1 for swimmer has only CSI badge" do
+      spc = SwimmerPresenceChecker.new(ganga, date_1819)
+      expect( spc.get_swimmer_current_badges().count ).to eq( 1 )
+    end
+
+    it "returns more than 1 for swimmer has at least CSI and FIN badges" do
+      spc = SwimmerPresenceChecker.new(leega, date_1819)
+      expect( spc.get_swimmer_current_badges().count ).to be > 1
     end
   end
   #-- -------------------------------------------------------------------------
@@ -51,8 +73,130 @@ describe SwimmerPresenceChecker, type: :strategy do
       expect( seasons ).to all(be_an_instance_of( Season ))
     end
 
+    it "returns 1 for swimmer has only CSI badge" do
+      spc = SwimmerPresenceChecker.new(ganga, date_1819)
+      expect( spc.get_swimmer_current_seasons().count ).to eq( 1 )
+    end
+
+    it "returns more than 1 for swimmer has at least CSI and FIN badges" do
+      spc = SwimmerPresenceChecker.new(leega, date_1819)
+      expect( spc.get_swimmer_current_seasons().count ).to be > 1
+    end
+
     it "returns the same number as current badges" do
       expect( subject.get_swimmer_current_seasons().count ).to eq( subject.get_swimmer_current_badges().count )
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+  
+  describe "#has_swimmer_attended_meeting" do
+    it "returns false for a new meeting" do
+      expect( subject.has_swimmer_attended_meeting(new_meeting) ).to eq(false)
+    end
+    
+    it "returns false for a new swimmer" do
+      spc = SwimmerPresenceChecker.new( new_swimmer, fix_date)
+      expect( spc.has_swimmer_attended_meeting(fix_meeting) ).to eq(false)
+    end
+    
+    it "return true for a swimmer with entries and w/o results" do
+      # Federico Attolini has only entries for csiprova1 2018 (Carpi)
+      attolini = Swimmer.find(51)
+      carpi = Meeting.find(18101)
+      spc = SwimmerPresenceChecker.new(attolini, date_1819)
+      expect( spc.has_swimmer_attended_meeting(carpi) ).to eq(true)
+    end
+
+    it "return true for a swimmer with results and w/o entries" do
+      # Leega has only results for Riccione 2018
+      riccione = Meeting.find(18224)
+      spc = SwimmerPresenceChecker.new(leega, date_1819)
+      expect( spc.has_swimmer_attended_meeting(riccione) ).to eq(true)
+    end
+
+    it "return true for a swimmer with only reservations" do
+      # Leega has only reservations for Ravenna 2018
+      ravenna = Meeting.find(18243)
+      spc = SwimmerPresenceChecker.new(leega, date_1819)
+      expect( spc.has_swimmer_attended_meeting(ravenna) ).to eq(true)
+    end
+
+    it "return false for a swimmer w/o results and w/o entries" do
+      # Leega hasn't entry or results for Viareggio 2018
+      viareggio = Meeting.find(18205)
+      spc = SwimmerPresenceChecker.new(leega, date_1819)
+      expect( spc.has_swimmer_attended_meeting(viareggio) ).to eq(false)
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+  
+  describe "#has_swimmer_swam_relay" do
+    it "returns false for a new meeting" do
+      expect( subject.has_swimmer_swam_relay( new_meeting ) ).to eq(false)
+    end
+
+    it "returns false for a new swimmer" do
+      spc = SwimmerPresenceChecker.new( new_swimmer, fix_date)
+      expect( spc.has_swimmer_swam_relay(fix_meeting) ).to eq(false)
+    end
+
+    it "return false for a swimmer that didn't swam on a relay" do
+      # Federico Attolini didn't swam at csiprova1 2018 (Carpi) and FIN meetings
+      attolini = Swimmer.find(51)
+      no_relays = Meeting.find( [18101, 18224, 18205, 18243].sample )
+      spc = SwimmerPresenceChecker.new(attolini, date_1819)
+      expect( spc.has_swimmer_swam_relay(no_relays) ).to eq(false)
+    end
+
+    it "return true for a swimmer who swam relay" do
+      # Leega swam CSI relays
+      yes_relays = Meeting.find( [18101, 18102, 18234].sample )
+      spc = SwimmerPresenceChecker.new(leega, date_1819)
+      expect( spc.has_swimmer_swam_relay(yes_relays) ).to eq(true)
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  describe "#count_swimmer_relays" do
+    it "returns 0 for a new meeting" do
+      expect( subject.count_swimmer_relays( new_meeting ) ).to eq(0)
+    end
+
+    it "returns 0 for a new swimmer" do
+      spc = SwimmerPresenceChecker.new( new_swimmer, fix_date)
+      expect( spc.count_swimmer_relays(fix_meeting) ).to eq(0)
+    end
+
+    it "return 0 for a swimmer that didn't swam on a relay" do
+      # Federico Attolini didn't swam at csiprova1 2018 (Carpi) and FIN meetings
+      attolini = Swimmer.find(51)
+      no_relays = Meeting.find( [18101, 18224, 18205, 18243].sample )
+      spc = SwimmerPresenceChecker.new(attolini, date_1819)
+      expect( spc.count_swimmer_relays(no_relays) ).to eq(0)
+    end
+
+    it "return > 0 for a swimmer who swam relay" do
+      # Leega swam CSI relays
+      yes_relays = Meeting.find( [18101, 18102, 18234].sample )
+      spc = SwimmerPresenceChecker.new(leega, date_1819)
+      expect( spc.count_swimmer_relays(yes_relays) ).to be > 0
+    end
+
+    it "return 1 for a swimmer who swam only one relay (CSI)" do
+      # Leega swam CSI relays
+      yes_relays = Meeting.find( [18101, 18102].sample )
+      spc = SwimmerPresenceChecker.new(leega, date_1819)
+      expect( spc.count_swimmer_relays(yes_relays) ).to eq(1)
+    end
+
+    it "return 2 for a swimmer who swam two relays (Parma)" do
+      # Leega swam CSI relays
+      yes_relays = Meeting.find( 18234 )
+      spc = SwimmerPresenceChecker.new(leega, date_1819)
+      expect( spc.count_swimmer_relays(yes_relays) ).to eq(2)
     end
   end
   #-- -------------------------------------------------------------------------
