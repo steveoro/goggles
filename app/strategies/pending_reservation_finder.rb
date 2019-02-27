7b65af8abe3d27f86b95f5e2bce62325e6550c0b
Marco Ligabue <leega@leegax.lan>
2019-02-27 22:47:36 +0100

    Query optimize and more specs on pending res strategy

--- app/strategies/pending_reservation_finder.rb
+++ app/strategies/pending_reservation_finder.rb
@@ -13,32 +13,36 @@ require 'wrappers/timing'
 class PendingReservationFinder
 
   # These must be initialized on creation:
-  attr_reader :user 
+  attr_reader :user, :meetings_ids, :seasons_ids, :pending_reservations, :seasons_found, :meetings_found 
 
   # These can be edited later on:
-  attr_accessor :meetings_ids, :team_affiliations_ids, :pending_reservations
+  #attr_accessor 
 
   # Initialization
   #
   def initialize( user )
-    @meetings_ids          = []
-    @team_affiliations_ids = []
-    @pending_reservations  = []
+    @meetings_ids         = []
+    @seasons_ids          = []
+    @pending_reservations = []
+    
+    @seasons_found  = false
+    @meetings_found = false
 
     @user = user
   end
   #-- --------------------------------------------------------------------------
   #++
 
-  # Find the manageable team affiliation of the given user
+  # Find the manageable seasons of the given user
   #
-  def find_manageable_affiliations( check_date = Date.today() )
-    @team_affiliations_ids = @user.
+  def find_manageable_seasons( check_date = Date.today() )
+    @seasons_found = true
+    @seasons_ids = @user.
       team_managers.
       joins( team_affiliation: :season ).
       where( ['seasons.end_date >= ? and seasons.begin_date <= ?', check_date, check_date]).
-      select( :team_affiliation_id ).
-      map{ |ta| ta.team_affiliation_id }
+      select( "team_affiliations.season_id" ).
+      map{ |tm| tm.season_id }
   end
   #-- -------------------------------------------------------------------------
   #++
@@ -47,34 +51,46 @@ class PendingReservationFinder
   # A meeting not closed is a meeting not in the past, withput results and with entry deadline not gone
   #
   def find_open_meetings( check_date = Date.today() )
+    @meetings_found = true
+    
+    # If no seasons_ids check out
+    if !@seasons_found
+      find_manageable_seasons()
+    end
+
     @meetings_ids = Meeting.
       where( are_results_acquired: false ).
+      where( season_id: @seasons_ids ).
       where(['meetings.header_date >= ? and (meetings.entry_deadline is null or meetings.entry_deadline > ?)', check_date, check_date] ).
       select( :id ).
       map{ |m| m.id }
   end
   #-- -------------------------------------------------------------------------
   #++
+
+  # Sets the given meeting id as the only searched for
+  #
+  def set_searched_meeting( meeting_id )
+    @meetings_ids = []
+    @meetings_found = true
+    @meetings_ids << meeting_id
+  end
   
   # Get the pending reservations (usefull datas)
   # Pending reservations are those not confirmed for meetings inj meetings_ids array
   #
   def get_pending_reservations()
-    # If no meetings_ids check out
-    if @meetings_ids.size == 0
+    # If not closed meetings scanned, check them out
+    if !@meetings_found
       find_open_meetings()
-    end
-
-    # If no team_affiliations_ids check out
-    if @team_affiliations_ids.size == 0
-      find_manageable_affiliations()
     end
     
     @pending_reservations = MeetingEventReservation.
-      joins( :swimmer, :badge, meeting_event: [:event_type, meeting_session: :swimming_pool], meeting: [season: [season_type: :federation_type]] ).
+      joins( :swimmer, meeting_event: [:event_type, meeting_session: :swimming_pool], meeting: [season: [season_type: :federation_type]] ).
       joins("INNER JOIN meeting_reservations on meeting_reservations.meeting_id = meeting_event_reservations.meeting_id and meeting_reservations.badge_id = meeting_event_reservations.badge_id").
-      where( is_doing_this: true, meeting_reservations: {has_confirmed: false}, meeting_id: @meetings_ids, badges: {team_affiliation_id: @team_affiliations_ids} ).
-      select( :meeting_id, 
+      where( is_doing_this: true, meeting_reservations: {has_confirmed: false}, meeting_id: @meetings_ids ).
+      select( :id,
+              :meeting_id, 
               :swimmer_id, 
               :suggested_minutes, 
               :suggested_seconds, 
@@ -83,10 +99,8 @@ class PendingReservationFinder
               "meetings.entry_deadline", 
               "meetings.description", 
               "swimmers.complete_name", 
-              "meeting_events.event_type_id", 
-              "event_types.code as event", 
-              "swimming_pools.pool_type_id", 
-              "badges.team_affiliation_id",
+              "event_types.code as event",
+              "meetings.season_id", 
               "federation_types.code as federation_code",
               #"'' as notes").  # Do it with a single query 
               "(SELECT concat_ws(' ', date_format(t_ms.scheduled_date, '%d/%m/%Y'), t_c.name, t_pt.code) as notes 
@@ -97,9 +111,9 @@ class PendingReservationFinder
                   INNER JOIN swimming_pools t_sp ON t_sp.id = t_ms.swimming_pool_id
                   INNER JOIN cities t_c ON t_c.id = t_sp.city_id
                   INNER JOIN pool_types t_pt ON t_pt.id = t_sp.pool_type_id
-                WHERE t_me.event_type_id = event_types.id 
+                WHERE t_mir.swimmer_id = swimmers.id 
+                  AND t_me.event_type_id = event_types.id 
                   AND t_pt.id = swimming_pools.pool_type_id
-                  AND t_mir.swimmer_id = swimmers.id 
                   AND t_mir.minutes = meeting_event_reservations.suggested_minutes  
                   AND t_mir.seconds = meeting_event_reservations.suggested_seconds
                   AND t_mir.hundreds = meeting_event_reservations.suggested_hundreds
--- spec/strategies/pending_reservation_finder_spec.rb
+++ spec/strategies/pending_reservation_finder_spec.rb
@@ -3,13 +3,8 @@ require 'rails_helper'
 
 describe PendingReservationFinder, type: :strategy do
 
-  let(:new_swimmer )      { create( :swimmer ) }
   let(:leega)             { Swimmer.find( 23 ).user }
 
-  let(:fix_date)          { Date.today() - 100 + (rand * 200).to_i }
-  let(:date_1819)         { Date.new(2018, 12, 31) } 
-
-  let(:new_meeting )      { create( :meeting, { header_date: Date.today + (rand * 20).to_i, are_results_acquired: false } ) }
   let(:acquired_meeting ) { create( :meeting, { are_results_acquired: true } ) }
   let(:past_meeting )     { create( :meeting, { header_date: Date.today - (rand * 20).to_i } ) }
   let(:closed_meeting )   { create( :meeting, { entry_deadline: Date.today - (rand * 20).to_i } ) }
@@ -19,33 +14,58 @@ describe PendingReservationFinder, type: :strategy do
   describe "[a well formed instance]" do
     it_behaves_like( "(the existance of a method returning an array)", [
       :meetings_ids, 
-      :team_affiliations_ids,
+      :seasons_ids,
       :pending_reservations,
       :find_open_meetings,
-      :find_manageable_affiliations,
+      :find_manageable_seasons,
       :get_pending_reservations
+    ])
+
+    it_behaves_like( "(the existance of a method returning a boolean value)", [
+      :seasons_found, 
+      :meetings_found
     ])
     
     it "returns empty meetings_ids on init" do
       expect( subject.meetings_ids.size ).to eq( 0 )
     end
     
-    it "returns empty team_affiliations_ids on init" do
-      expect( subject.team_affiliations_ids.size ).to eq( 0 )
+    it "returns empty seasons_ids on init" do
+      expect( subject.seasons_ids.size ).to eq( 0 )
     end
     
     it "returns empty pending_reservations on init" do
       expect( subject.pending_reservations.size ).to eq( 0 )
+    end
+    
+    it "returns a false seasons found tracer" do
+      expect( subject.seasons_found ).to eq( false )
+    end
+    
+    it "returns a false meetings found tracer" do
+      expect( subject.meetings_found ).to eq( false )
     end
   end
   #-- -------------------------------------------------------------------------
   #++
   
   describe "#find_open_meetings" do
+    it "sets true found meetingsand seasons tracer" do
+      expect( subject.seasons_found ).to eq( false )
+      expect( subject.meetings_found ).to eq( false )
+      subject.find_open_meetings()
+      expect( subject.seasons_found ).to eq( true )
+      expect( subject.meetings_found ).to eq( true )
+    end
+    
     it "finds at least one new meeting" do
+      ms = subject.find_manageable_seasons(Date.new(2019, 02, 26))
+      new_meeting = create( :meeting, { header_date: Date.today + (rand * 20).to_i, are_results_acquired: false , entry_deadline: Date.today + (rand * 20).to_i, season_id: ms.sample }) 
+ 
       expect( new_meeting.header_date ).to be > Date.today()
       expect( new_meeting.are_results_acquired ).to eq( false )
-      expect( subject.find_open_meetings.size ).to be >= 1
+      expect( ms.include?( new_meeting.season_id )).to eq( true )
+      expect( subject.find_open_meetings(Date.new(2019, 02, 26)).size ).to be >= 1
     end
 
     it "doesn't find meeting with acquired results" do
@@ -69,19 +89,51 @@ describe PendingReservationFinder, type: :strategy do
   #-- -------------------------------------------------------------------------
   #++
   
-  describe "#find_manageable_affiliations" do
-    it "find at least 2 manageable team affiliations for Leega in 2018/2019" do
-      expect( subject.find_manageable_affiliations(Date.new(2019, 02, 26)).count).to be >= 2
+  describe "#set_searched_meetings" do
+    it "returns an array" do
+      expect( subject.set_searched_meeting((rand * 20000).to_i)).to be_a_kind_of(Array)
     end
-    it "find no more than 3 manageable team affiliations for Leega in 2017/2018" do
-      expect( subject.find_manageable_affiliations(Date.new(2018, 02, 26)).count).to be <= 3
+
+    it "populates meetings_ids" do
+      expect( subject.meetings_found ).to eq( false )
+      expect( subject.meetings_ids.size ).to eq( 0 )
+      subject.set_searched_meeting((rand * 20000).to_i)
+      expect( subject.meetings_found ).to eq( true )
+      expect( subject.meetings_ids.size ).to eq( 1 )
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+  
+  describe "#find_manageable_seasons" do
+    it "sets true found seasons tracer" do
+      expect( subject.seasons_found ).to eq( false )
+      subject.find_manageable_seasons()
+      expect( subject.seasons_found ).to eq( true )
     end
     
+    it "find at least 2 manageable seasons for Leega in 2018/2019" do
+      expect( subject.find_manageable_seasons(Date.new(2019, 02, 26)).count).to be >= 2
+    end
+    
+    it "find no more than 3 manageable seasons for Leega in 2017/2018" do
+      expect( subject.find_manageable_seasons(Date.new(2018, 02, 26)).count).to be <= 3
+    end
   end
   #-- -------------------------------------------------------------------------
   #++
   
   describe "#get_pending_reservations" do
+    it "returns an hash responding to needed methods" do
+      pr = subject.get_pending_reservations()
+      pr.each do |r|
+        expect( r.id ).to be > 0
+        expect( r.meeting_id ).to be > 0
+        expect( r.season_id ).to be > 0
+        expect( r.swimmer_id ).to be > 0
+      end
+    end
+    
     it "returns only reservations for searched meetings" do
       pr = subject.get_pending_reservations()
       m = subject.meetings_ids
@@ -92,9 +144,9 @@ describe PendingReservationFinder, type: :strategy do
 
     it "returns only reservations for manageable team affiliations" do
       pr = subject.get_pending_reservations()
-      ta = subject.team_affiliations_ids
+      s = subject.seasons_ids
       pr.each do |r|
-        expect( ta.include?( r.team_affiliation_id )).to eq( true )
+        expect( s.include?( r.season_id )).to eq( true )
       end
     end  
   end
