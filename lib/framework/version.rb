e8107b65ba95ca9691e87b2571e72aaa0fc5c51e
Steve Alloro <steve.alloro@gmail.com>
2019-03-16 19:10:49 +0100

    security update

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '6'
 
   # Minor version.
-  MINOR   = '379'
+  MINOR   = '380'
 
   # Current build version.
-  BUILD   = '20190217'
+  BUILD   = '20190316'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
